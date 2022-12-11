6829051e1c5aff7053ebf0b297592b1858908c5a
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2022-12-11 13:18:20 +0600

    ACMP

--- ACMP/0108.cpp
+++ ACMP/0108.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 
 int main() {
-   int x, y , z
-   
-   cin >> 
-   
+	setlocale(LC_ALL , "Russian");
+	int a;
+	cin >> a;
+	cout << a;
+	
+	
 	
 	
 	return 0;
---
+++ ACMP/0766.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, m , k;
+    cin >> n >> m >> k;
+    
+    
+    if (n * m >= k) cout << "YES";
+    else  cout << "NO";
+    
+    
+    
+	return 0;
+}
