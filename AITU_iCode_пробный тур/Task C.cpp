1390df9b3c3c9fc7a6e9fc82d7e54ca62249985a
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2022-12-12 23:12:41 +0600

    iCode

---
+++ AITU_iCode_пробный тур/Task C.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b;
+	cin >> a >> b;
+	if(a > 9 || b > 9) {
+		cout << "-1";
+	}
+	else cout << a * b;
+	  
+	
+	
+	
+	
+	
+	return 0;
+}
