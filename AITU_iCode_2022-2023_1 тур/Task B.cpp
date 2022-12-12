f186a56ba1d334e7c7b8ced3342619b8507b8dc3
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2022-12-12 22:16:01 +0600

    iCode

---
+++ AITU_iCode_2022-2023_1 тур/Task B.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	long long int a, b, c, a1, b1, c1;
+	cin >> a >> b >> c;
+	a1 = a / 2;
+	b1 = b / 2;
+	c1 = c;
+	cout << min(a1, min(b1, c1));
+	
+	
+	
+	
+	
+	
+	return 0;
+}
