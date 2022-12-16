f4f90066b2872b44d49f059153e88875495d023f
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2022-12-16 23:03:56 +0600

    ACMP

---
+++ ACMP/1119.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int x1, x2, y1, y2;
+	cin >> x1 >> y1 >> x2 >> y2;
+	if(y1 == y2 || x1 == x2 ) {
+		cout << "YES";
+	}
+	else cout << "NO";
+	
+	
+	
+	
+	
+	
+	return 0;
+}
