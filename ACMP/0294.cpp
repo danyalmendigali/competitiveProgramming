b6c69358d89b6cd403f8b51948d13a69c07dbbc2
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-03-02 22:27:20 +0600

    ACMP

---
+++ ACMP/0294.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int k1, k2, l1, l2, m1, m2, cl1, cl2;
+	
+	cin >> k1 >> l1 >> m1;
+	cin >> k2 >> l2 >> m2;
+	
+	cl1 = k1 * l1 / 100;
+	cl2 = k2 * l2 / 100;
+	
+	k1 -= cl1;
+	k2 -= cl2;
+	
+	int s = cl1 * m1 + cl2 * m2;
+	if(k1 > k2) s += (k1 - k2) * m1;
+	else s += (k2 - k1) * m2;
+	
+	cout << s;
+	
+	
+	
+	
+		
+	return 0;
+}
