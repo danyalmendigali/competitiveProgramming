5e8c23517bd0d0f36569bd0af57759e2fb95a5d0
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-03-03 20:50:18 +0600

    Codeforces

--- Codeforces/4A.cpp
+++ Codeforces/4A.cpp
@@ -4,11 +4,15 @@ using namespace std;
 int main() {
 	int n;
 	cin >> n;
-	if(n % 2 == 0) {
+	if (n == 2) {
+		cout << "NO";
+	}
+	else if(n % 2 == 0 ) {
 		cout << "YES";
 	}
-	else cout << "NO";
-	
+	else {
+		cout << "NO";
+	}
 	
 	
 	
