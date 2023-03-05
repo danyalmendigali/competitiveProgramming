bec182626ece51306d1a3c7e4e60da0416c946c0
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-03-05 22:36:29 +0600

    Codeforces

---
+++ Codeforces/1760A.cpp
@@ -0,0 +1,29 @@
+#include <set>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	for(int i = 0; i < n; i++) {
+		int a, b, c;
+		cin >> a >> b >> c;
+		int maxA, minA, g;
+		maxA = max(a, max(b, c));
+		minA = min(a, min(b, c));
+		g = (a + b + c) - (maxA + minA);
+		cout << g << endl;
+		
+	}
+	
+	
+	
+	
+	return 0;
+}
---
+++ Codeforces/1772A.cpp
@@ -0,0 +1,30 @@
+#include <set>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	for(int i = 0; i <= n; i++) {
+		string s;
+		cin >> s;
+		for(int j = 0; j < s.size(); i++) {
+			if(s[j] >= '0' && s[j] <= '9' ) {
+				int a;
+				a += s[j];
+			}
+		}
+	}
+	
+	
+	
+	
+	
+	return 0;
+}
