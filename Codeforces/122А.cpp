1eda748dcb10d2e3d29875c5125986c9892534ac
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-03-05 14:17:14 +0600

    Codeforces

---
+++ Codeforces/122А.cpp
@@ -0,0 +1,29 @@
+#include <cstdio>
+#include <cstdlib>
+#include <map>
+#include <set>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int l;
+	cin >> l;
+	
+	if(l % 4 == 0 || l % 7 == 0 || l % 47 == 0 || l % 74 == 0 || l % 477 == 0) {
+		cout << "YES";
+	}
+	else cout << "NO";
+	
+	
+	
+	
+	
+	
+	return 0;
+}
