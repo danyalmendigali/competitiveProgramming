c6eef24faea20eb10fdf8083094be1069e6abf77
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-03-05 21:19:06 +0600

    Codeforces

---
+++ Codeforces/1186A.cpp
@@ -0,0 +1,30 @@
+#include <cstdio>
+#include <cstdlib>
+#include <map>
+#include <set>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+
+int main() {
+	
+	int n, m, k;
+	cin >> n >> m >> k;
+	
+	if(n <= m && n <= k) {
+		cout << "YES";
+	}
+	
+	else cout << "NO";
+	
+	
+	
+	
+	return 0;
+}
