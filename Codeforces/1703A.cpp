aff5d99aa23cb4b17ed71c433e49a698b05ec7af
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-03-05 17:50:02 +0600

    Codeforces

--- Codeforces/1703A.cpp
+++ Codeforces/1703A.cpp
@@ -1,14 +1,20 @@
-#include <iostream>
+#include <set>
+#include <cmath>
+#include <vector>
 #include <string>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+
 using namespace std;
 
 int main() {
 	int n;
-	string s;
 	cin >> n;
 	for(int i = 0; i < n; i++) {
+		string s;
 		cin >> s;
-		if(s == "Yes" || s == "YES" || s == "yes" || s == "yES" || s == "YeS" || s == "yeS" || s = "yEs" || s == "YEs" ) {
+		if(s == "Yes" || s == "YES" || s == "yes" || s == "yES" || s == "YeS" || s == "yeS" || s == "yEs" || s == "YEs" ) {
 			cout << "YES" << endl;
 		}
 		
