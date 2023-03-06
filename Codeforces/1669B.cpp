86e62755b2f5545275ba67ee11006a1a8ba4f1bc
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-03-06 18:51:52 +0600

    Codeforces

---
+++ Codeforces/1669B.cpp
@@ -0,0 +1,38 @@
+#include <cstdio>
+#include <cstdlib>
+#include <map>
+#include <set>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	for(int i = 0; i < n; i++) {
+		int s;
+		cin >> s;
+		int a[s];
+		for(int i = 0; i < s; i++) {
+			cin >> a[i];
+			int a = 0;
+			if(a[i] == a[i + 1]) {
+				a++;
+			}
+		}
+		
+		if(a)
+	}
+	
+	
+	
+	
+	
+	
+	return 0;
+}
---
+++ Codeforces/281A.cpp
@@ -0,0 +1,24 @@
+#include <cstdio>
+#include <cstdlib>
+#include <map>
+#include <set>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	string s;
+	cin >> s;
+	s[0] = toupper(s[0]);
+	cout << s;
+	
+	
+	
+	
+	return 0;
+}
