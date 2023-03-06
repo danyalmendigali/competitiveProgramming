e4b586278c87ff32184c6d5dfca16c823a220378
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-03-06 17:39:39 +0600

    Codeforces

---
+++ Codeforces/1772A.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+#include <cstdlib>
+#include <map>
+#include <set>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	for(int i = 0; i < n; i++) {
+		char b;
+		int f, a, c;
+		cin >> a >> b >> c;
+		f = a + c;
+		cout << f << endl;
+	}
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
