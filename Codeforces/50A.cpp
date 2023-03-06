06c23f78ac60e8ff30f36a3def033d230781e40e
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-03-06 23:09:40 +0600

    Codeforces

---
+++ Codeforces/50A.cpp
@@ -0,0 +1,22 @@
+#include <set>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+	int n, m;
+	cin >> n >> m;
+	cout << n * m / 2;
+	
+	
+	
+	
+	
+	
+	return 0;
+}
