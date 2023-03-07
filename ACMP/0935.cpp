196a3b43c0724527c01eaeec0702334968308a66
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-03-07 22:51:00 +0600

    ACMP

--- ACMP/0935.cpp
+++ ACMP/0935.cpp
@@ -11,11 +11,11 @@
 using namespace std;
 
 int main() {
-	int x1, y1, x2, y2, a, b;
+	int x1, y1, x2, y2, a1, b1;
 	cin >> x1 >> y1 >> x2 >> y2;
-	a = x1 + y1;
-	b = x2 + y2;
-	if(a % 2 == b % 2) cout << "YES";
+	a1 = x1 + y1;
+	b1= x2 + y2;
+	if(a1 % 2 == b1 % 2) cout << "YES";
 	else cout << "NO";
 	
 	
