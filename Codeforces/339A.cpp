86b6d505509e1713f215bfe7ddaa757ca4a1971f
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-03-07 22:24:52 +0600

    Codeforces

--- Codeforces/339A.cpp
+++ Codeforces/339A.cpp
@@ -13,11 +13,11 @@
 using namespace std;
 
 int main() {
-	string s;
-	cin >> s;
+	string b;
+	cin >> b;
 	vector <int> a;
-	for(int i = 0; i < s.size(); i+=2) {
-		a.push_back(s 	[i] - 48);
+	for(int i = 0; i < b.size(); i+=2) {
+		a.push_back(b[i] - 48);
 	}
 	
 	sort(a.begin(), a.end());
