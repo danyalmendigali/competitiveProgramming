a7986b2710a457ce128a9918ac205e89c4fc9fe1
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-03-07 20:40:12 +0600

    Codefoces

---
+++ Codeforces/263A.cpp
@@ -0,0 +1,43 @@
+#include <cstdio>
+#include <cstdlib>
+#include <map>
+#include <set>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+const int n = 5;
+int main() {
+	int arr[n][n];
+	int x, y, s;
+	for(int i = 0; i < n; i++) {
+		for(int j = 0; j < n; j++) {
+			cin >> arr[i][j];		
+			if(arr[i][j] == 1) {
+				x = i;
+				y = j;
+				s = x + y;
+				
+				
+	  }
+     }
+   }
+	cout << abs(x - 2) + abs(y - 2);
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
