aab36f98cc778ad72393476aee876ccbe0479fa7
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-03-08 20:10:58 +0600

    Codeforces

---
+++ ACMP/1850.cpp
@@ -0,0 +1,25 @@
+#include <map>
+#include <set>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <cctype>
+
+using namespace std;
+
+int main() {
+	long long int n;
+	long double a;
+	cin >> n;
+	a = n * 1.8 + 32;
+	cout << a;
+	
+	
+	
+	
+	
+	return 0;
+}
