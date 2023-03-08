63e1dd3e4d88f2fd4256344f412ffb40a8a60ba3
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-03-08 23:19:58 +0600

    Codeforces

---
+++ ACMP/1754.cpp
@@ -0,0 +1,24 @@
+#include <map>
+#include <set>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <cctype>
+
+using namespace std;
+
+int main() {
+	int a, b, c;
+	cin >> a >> b >> c;
+	c -= a;
+	cout << c / b;
+	
+	
+	
+	
+	
+	return 0;
+}
