962b430401b3e1df47ccfb153002314772fe04d2
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-03-08 20:03:58 +0600

    ACMP

---
+++ ACMP/1796.cpp
@@ -0,0 +1,102 @@
+#include <map>
+#include <set>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <cctype>
+
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	if(n == 0) {
+		cout << "###" << endl;
+		cout << "# #" << endl;
+		cout << "# #" << endl;
+		cout << "# #" << endl;
+		cout << "###" << endl;
+	}
+	
+	if(n == 1) {
+	    cout << "  #" << endl;
+		cout << " ##" << endl;
+		cout << "# #" << endl;
+		cout << "  #" << endl;
+		cout << "  #" << endl;
+	}
+	
+	if(n == 2) {
+	    cout << "###" << endl;
+		cout << "  #" << endl;
+		cout << "###" << endl;
+		cout << "#  " << endl;
+		cout << "###" << endl;
+	}
+	
+	if(n == 3) {
+	    cout << "###" << endl;
+		cout << "  #" << endl;
+		cout << "###" << endl;
+		cout << "  #" << endl;
+		cout << "###" << endl;
+	}
+	
+	if(n == 4) {
+	    cout << "# #" << endl;
+		cout << "# #" << endl;
+		cout << "###" << endl;
+		cout << "  #" << endl;
+		cout << "  #" << endl;
+	}
+	
+	if(n == 5) {
+	    cout << "###" << endl;
+		cout << "#  " << endl;
+		cout << "###" << endl;
+		cout << "  #" << endl;
+		cout << "###" << endl;
+	}
+	
+	if(n == 6) {
+	    cout << "###" << endl;
+		cout << "#  " << endl;
+		cout << "###" << endl;
+		cout << "# #" << endl;
+		cout << "###" << endl;
+	}
+	
+	if(n == 7) {
+	    cout << "###" << endl;
+		cout << "  #" << endl;
+		cout << "  #" << endl;
+		cout << "  #" << endl;
+		cout << "  #" << endl;
+	}
+	
+	if(n == 8) {
+	    cout << "###" << endl;
+		cout << "# #" << endl;
+		cout << "###" << endl;
+		cout << "# #" << endl;
+		cout << "###" << endl;
+	}
+	
+	if(n == 9) {
+	    cout << "###" << endl;
+		cout << "# #" << endl;
+		cout << "###" << endl;
+		cout << "  #" << endl;
+		cout << "###" << endl;
+	}
+	
+
+	
+	
+	
+	return 0;
+       
+}
