43baeb4bed651d4c81288de398123dafa8d1ab57
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-03-09 20:28:01 +0600

    ACMP

--- ACMP/1216.cpp
+++ ACMP/1216.cpp
@@ -4,10 +4,12 @@ using namespace std;
 int main() {
 	int a[1001], i, n, l, r, k;
 	cin >> n;
+	
 	for(i = 1; i <= n; i++)
 	cin >> a[i];
 	cin >> l >> r;
 	k = l;
+	
 	for(i = l + 1; i <= r; i++)
 	if(a[i] > a[k]) k = i;
 	cout << a[k] << " " << k;
