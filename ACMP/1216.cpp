08abc7a0e68700d43ffe9dd87e40d5deedb770e7
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-03-09 23:03:31 +0600

    ACMP

--- ACMP/1216.cpp
+++ ACMP/1216.cpp
@@ -1,17 +1,30 @@
+#include <map>
+#include <set>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <cstring>
 #include <iostream>
+#include <algorithm>
+#include <cctype>
+
 using namespace std;
 
 int main() {
 	int a[1001], i, n, l, r, k;
-	cin >> n;
+	  cin >> n;
 	
-	for(i = 1; i <= n; i++)
-	cin >> a[i];
-	cin >> l >> r;
-	k = l;
+	for(i = 1; i <= n; i++) {
+	  cin >> a[i];
+  }
+	  cin >> l >> r;
+	  k = l;
 	
-	for(i = l + 1; i <= r; i++)
-	if(a[i] > a[k]) k = i;
+    for(i = l + 1; i <= r; i++) {	
+	   if(a[i] > a[k]) {
+	      k = i;
+    }
+}
 	cout << a[k] << " " << k;
 
 	
