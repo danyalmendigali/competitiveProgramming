e3e9da538d7091e6e247d689c34ebc0a84da6fa8
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-04-08 23:09:18 +0600

    Informatics

---
+++ Informatics/Задача №105.cpp
@@ -0,0 +1,22 @@
+#include <map>
+#include <set>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+#include <cctype>
+
+using namespace std;
+
+int main() {
+	string s1, s2;
+	getline (cin, s1);
+	getline(cin, s2);
+	if(s1 == s2) cout << "yes";
+	else cout << "no";
+	
+	
+	return 0;
+}
