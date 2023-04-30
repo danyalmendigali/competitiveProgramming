94d6a93003e8acca6b2a83c2305cbd7d64a07b07
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-04-30 22:54:26 +0600

    005

--- stl_task/005.cpp
+++ stl_task/005.cpp
@@ -1,1 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+int main() {
+    cin.tie(NULL);
+    setlocale(LC_ALL, "Russian");
+    int n, l = 0;
+    cin >> n;
+    vector <int> myVector(n);
+
+    for(int i = 0; i < n; i++) {
+        cin >> myVector[i];
+        if(myVector[i] % 3 == 0) {
+            l += myVector[i];
+        }
+    }
+
+    cout << l;
+
+
+
+
+
+
+
+
+
+
+
+   return 0;
+}
