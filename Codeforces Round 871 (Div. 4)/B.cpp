e5e99f211b9c9da48ddb5aac9f9706c0fa3492cb
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-05-16 22:30:57 +0600

    Div. 4

--- Codeforces Round 871 (Div. 4)/B.cpp
+++ Codeforces Round 871 (Div. 4)/B.cpp
@@ -4,35 +4,38 @@ using namespace std;
 #define ll long long
 #define array arr
 
-int main() {
-     ios::sync_with_stdio(false);
-     cin.tie();
-
-     int l;
-     cin >> l;
-     for(int i = 0; i < l; i++) {
+// Функция решения задачи
+void solve() {
+    int l;
+    cin >> l; // Вводим количество тестовых случаев
+    for (int i = 0; i < l; i++) {
         int n;
-        cin >> n;
+        cin >> n; // Вводим размер массива
         int arr[n];
-        for(int i = 0; i < n; i++) {
-            cin >> arr[i];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i]; // Вводим элементы массива
         }
         int max_count = 0;
         int count = 0;
-        for(int i = 0; i < n; i++) {
-            if(arr[i] == 0) {
-              count++;
-                if(count > max_count) {
-                    max_count = count;
+        for (int i = 0; i < n; i++) {
+            if (arr[i] == 0) {
+                count++; // Увеличиваем счетчик нулевых элементов
+                if (count > max_count) {
+                    max_count = count; // Если текущий счетчик больше максимального, обновляем максимальный счетчик
                 }
-            }
-            else {
-                count = 0;
+            } else {
+                count = 0; // Если текущий элемент не равен нулю, сбрасываем счетчик
             }
         }
-        cout << max_count << endl;
+        cout << max_count << endl; // Выводим максимальное количество последовательных нулей
+    }
+}
 
-      }
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie();
 
-  return 0;
+    solve(); // Вызываем функцию решения задачи
+
+    return 0;
 }
--- Codeforces Round 871 (Div. 4)/C.cpp
+++ Codeforces Round 871 (Div. 4)/C.cpp
@@ -7,38 +7,37 @@ using namespace std;
 
 void solve() {
     int t;
-    cin >> t;
-    while(t--) {
+    cin >> t; // Вводим количество тестовых случаев
+    while(t--) { // Цикл для каждого тестового случая
         int n;
-        cin >> n;
-        ll ans1 = INT_MAX, ans2 = INT_MAX, ans3 = INT_MAX;
+        cin >> n; // Вводим количество элементов в текущем случае
+        ll ans1 = INT_MAX, ans2 = INT_MAX, ans3 = INT_MAX; // Инициализация переменных-ответов значением INT_MAX (максимальное значение для int)
         ll a;
         string b;
         for(int i = 0; i < n; i++) {
-            cin >> a >> b;
+            cin >> a >> b; // Вводим пару чисел (a) и строку (b)
             if(b == "11")
-                ans1 = min(ans1, a);
+                ans1 = min(ans1, a); // Если b равно "11", то обновляем ans1, выбирая минимальное значение между текущим ans1 и a
 
             else if(b == "01")
-                ans2 = min(ans2, a);
+                ans2 = min(ans2, a); // Если b равно "01", то обновляем ans2, выбирая минимальное значение между текущим ans2 и a
 
             else if(b == "10")
-                ans3 = min(ans3, a);
+                ans3 = min(ans3, a); // Если b равно "10", то обновляем ans3, выбирая минимальное значение между текущим ans3 и a
         }
-        if(ans1 == INT_MAX&& (ans2 == INT_MAX || ans3 == INT_MAX)) {
-            cout << -1 << "\n";
+        if(ans1 == INT_MAX && (ans2 == INT_MAX || ans3 == INT_MAX)) {
+            cout << -1 << "\n"; // Если ans1 равен INT_MAX и (ans2 равен INT_MAX или ans3 равен INT_MAX), то выводим -1 и переходим к следующему тестовому случаю
             continue;
         }
         if(ans1 < (ans2 + ans3)) {
-            cout << ans1;
+            cout << ans1; // Если ans1 меньше суммы ans2 и ans3, то выводим ans1
         }
         else {
-            cout << ans2 + ans3;
+            cout << ans2 + ans3; // Иначе выводим сумму ans2 и ans3
         }
         cout << "\n";
     }
 }
-
 
 int main() {
     ios::sync_with_stdio(false);
@@ -46,14 +45,5 @@ int main() {
 
     solve();
 
-
-
-
-
-
-
-
-
-
-   return 0;
+    return 0;
 }
