bb855eaafcdf00db19fef7d7a72e7d48b4393cfc
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-05-16 23:35:16 +0600

    Codeforces

---
+++ Codeforces/443A.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+
+void solve() {
+    string input;
+    getline(cin, input);
+
+    unordered_set<char> letters;
+    for (size_t i = 1; i < input.length() - 1; i += 3) {
+        letters.insert(input[i]);
+    }
+
+    cout << letters.size() << std::endl;
+
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+
+    solve();
+
+
+   return 0;
+}
