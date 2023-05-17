996a4f7546923f27d88730455079288c7fa115d6
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-05-17 20:37:22 +0600

    Codeforces

--- Codeforces/723A.cpp
+++ Codeforces/723A.cpp
@@ -5,7 +5,7 @@ using namespace std;
 #define ar array
 
 void solve() {
-    int x1, x2, x3;
+    /*int x1, x2, x3;
     cin >> x1 >> x2 >> x3;
     int min_1 = min(x1, min(x2, x3));
     int max_1 = max(x1, max(x2, x3));
@@ -14,7 +14,14 @@ void solve() {
     int b, c, f;
     b = max_1 - avarage;
     c = avarage - min_1;
-    cout << b + c;
+    cout << b + c;*/
+
+    int arr[2];
+    for(int i = 0; i < 3; i++) {
+        cin >> arr[i];
+    }
+    sort(arr, arr + 3);
+    cout << (arr[1] - arr[0] + arr[2] - arr[1]) - 1;
 
 }
 
