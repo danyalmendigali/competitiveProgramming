4bd5c85a81e992c251725aae2146158724eba7ec
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-05-21 01:01:38 +0600

    Codeforces

---
+++ Codeforces/1095A.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+
+void solve() {
+    int n;
+    string t;
+    cin >> n >> t;
+
+    string s = "";
+    int len = 0;
+    int count = 1;
+
+    while (len < n) {
+        s += t[len];
+        len += count;
+        count++;
+    }
+
+    cout << s << endl;
+
+
+
+
+
+}
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    solve();
+
+
+
+
+
+
+   return 0;
+}
+
+
+
+
