44c5b021699dc45c3c93516a8cf4e2aae29577ae
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-05-22 16:24:44 +0600

    Codeforces

--- Codeforces/34A.cpp
+++ Codeforces/34A.cpp
@@ -4,33 +4,30 @@ using namespace std;
 #define ll long long
 #define ar array
 
-
-pair <int, int> findMinimumDifference(const vector <int>& arr) {
-    int minDiff = numeric_limits<int>::max();
-    pair <int, int> pair;
-
-    for(int i = 0; i < arr.size() - 1; i++) {
-        int diff = abs(arr[i + 1] - arr[i]);
-
-        if(diff < minDiff) {
-            minDiff = diff;
-            pair = make_pair(i, i + 1);
-        }
-    }
-    return pair;
-
-}
-
 void solve() {
     int n;
     cin >> n;
-    vector <int> array (n);
+    vector <int> heights (n);
     for(int i = 0; i < n; i++) {
-        cin >> array[i];
+        cin >> heights[i];
+    }
+    int minDiff = abs(heights[0] - heights[n - 1]);
+    int soldier1 = 1;
+    int soldier2 = n;
+
+    for(int i = 0; i < n - 1; i++) {
+       int diff = abs(heights[i] - heights[i + 1]);
+        if (diff < minDiff) {
+            minDiff = diff;
+            soldier1 = i + 1;
+            soldier2 = i + 2;
+        }
     }
 
-    pair <int, int> result = findMinimumDifference(array);
-    cout << result.first << " " <<  result.second << endl;
+    cout << soldier1 << " " << soldier2 << endl;
+
+
+
 }
 
 int main() {
