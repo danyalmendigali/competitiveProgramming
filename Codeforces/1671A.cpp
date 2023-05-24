aa2ab66bb0ed87e8c09419b9749babd69e4ad203
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-05-24 20:58:38 +0600

    Codeforces

---
+++ Codeforces/1671A.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool isConcatenation(const string& str) {
+    regex pattern("^(a{2,3}|b{2,3})+$");
+    return regex_match(str, pattern);
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int t;
+    cin >> t;
+
+    while(t--) {
+        string s;
+        cin >> s;
+      if(isConcatenation(s)) {
+        cout << "YES" << endl;
+      }
+      else {
+        cout << "NO" << endl;
+      }
+    }
+
+
+    return 0;
+}
+
