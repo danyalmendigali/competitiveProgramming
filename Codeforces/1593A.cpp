4dd6ff2ff5e4bea9bd3a8f063f3049bfe7d6037f
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-05-25 21:05:55 +0600

    Codeforces

--- Codeforces/1593A.cpp
+++ Codeforces/1593A.cpp
@@ -5,12 +5,27 @@ using namespace std;
 #define ar array
 
 int solve(ll int& a, ll int& b, ll int& c) {
-        long mx = (a > b) ? a : b; mx = (mx > c) ? mx : c;
-        bool tie = (((a == mx) + (b == mx) + (c == mx)) > 1);
-        long u = (a == mx) ? tie : (mx + 1 - a);
-        long v = (b == mx) ? tie : (mx + 1 - b);
-        long w = (c == mx) ? tie : (mx + 1 - c);
-        cout << u << " " << v << " " << w << endl;
+        // Находим максимальное значение из a, b и c и сохраняем в переменной mx
+    long mx = (a > b) ? a : b;
+    mx = (mx > c) ? mx : c;
+
+    // Проверяем, есть ли ничья (если хотя бы два числа равны максимальному)
+    bool tie = (((a == mx) + (b == mx) + (c == mx)) > 1);
+
+    // Вычисляем значение переменной u
+    // Если a равно mx, присваиваем значение tie, иначе mx + 1 - a
+    long u = (a == mx) ? tie : (mx + 1 - a);
+
+    // Вычисляем значение переменной v
+    // Если b равно mx, присваиваем значение tie, иначе mx + 1 - b
+    long v = (b == mx) ? tie : (mx + 1 - b);
+
+    // Вычисляем значение переменной w
+    // Если c равно mx, присваиваем значение tie, иначе mx + 1 - c
+    long w = (c == mx) ? tie : (mx + 1 - c);
+
+    // Выводим значения u, v и w
+    cout << u << " " << v << " " << w << endl;
 }
 
 int main() {
