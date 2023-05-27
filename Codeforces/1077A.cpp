37ef47a96abaddd8304906634fe679d9a59e63c3
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-05-28 00:13:34 +0600

    Codeforces

--- Codeforces/1077A.cpp
+++ Codeforces/1077A.cpp
@@ -4,7 +4,7 @@ using namespace std;
 #define ll long long
 #define ar array
 
-void solve() {
+/*
     int t;
     cin >> t;
     while(t--) {
@@ -26,6 +26,36 @@ void solve() {
       }
 
     }
+    */
+
+void solve() {
+
+    int t;
+    cin >> t;
+    while(t--) {
+        ll int a, b, k;
+        cin >> a >> b >> k;
+    if(k % 2 != 0) {
+    int g, h;
+     g = k / 2 + 1;
+     h = k - g;
+      cout << a * g - b * h;
+      cout << endl;
+    }
+
+    else {
+    int g, h;
+      g = k / 2;
+      h = k - g;
+       cout << a * g - b * h;
+       cout << endl;
+      }
+    }
+
+
+
+
+
 
 }
 
