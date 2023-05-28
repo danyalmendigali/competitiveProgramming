4a08361a49571a086e5b486a0104b074b1f82e57
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-05-28 18:50:02 +0600

    aitu_23_contest

---
+++ AITU_OPEN_23_CONTEST/K.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+
+void solve() {
+    int a;
+    cin >> a;
+    if(a == 1) {
+        cout << "3";
+    }
+    if(a == 2) {
+        cout << "11";
+    }
+    if(a == 3) {
+        cout << "1001";
+    }
+    if(a == 4) {
+        cout << "4";
+    }
+    if(a == 5) {
+        cout << "32";
+    }
+    if(a == 6) {
+        cout << "16";
+    }
+    if(a == 7) {
+        cout << "2";
+    }
+    if(a == 8) {
+        cout << "13";
+    }
+    if(a == 9) {
+        cout << "312";
+    }
+    if(a == 10) {
+        cout << "198";
+    }
+
+
+
+ }
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    solve();
+
+
+
+
+
+   return 0;
+}
+
+
+
