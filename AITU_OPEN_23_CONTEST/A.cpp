5d737666b3437fab295ab972d1f9f5636b3ae372
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-05-28 18:48:09 +0600

    aitu_23_contest

---
+++ AITU_OPEN_23_CONTEST/A.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+
+void solve() {
+     int a, b, c;
+     cin >> a >> b >> c;
+     if(a + b <= 13 && a + c <= 13) {
+        cout << "YES" << endl;
+     }
+     else {
+        cout << "NO" << endl;
+     }
+
+
+
+
+
+
+ }
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    solve();
+
+
+
+
+
+   return 0;
+}
+
