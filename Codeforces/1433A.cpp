e59d9bc6cca62ce4663faebb6577b5b527468932
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-18 23:52:05 +0600

    Codeforces

--- Codeforces/1433A.cpp
+++ Codeforces/1433A.cpp
@@ -2,36 +2,33 @@
 #include <windows.h>
 using namespace std;
 
-
+#define FIO ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define PI 3.1415926535897932384626
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+#define sz(x) x.size()
 
 int solve(int x)
 {
     int cnt = ((x % 10) - 1) * 10;
 
     string s = to_string(x);
-    cnt += (s.size() * (s.size() + 1) / 2);
-
+    cnt += (sz(s) * (sz(s) + 1) / 2);
 
     return cnt;
 }
 
 
 int main() {
+    FIO;
 
     int t;
     cin >> t;
 
     while(t--) {
-      int x;
-      cin >> x;
-      cout << solve(x) << "\n";
-
+        int x;
+        cin >> x;
+        cout << solve(x) << "\n";
     }
 
-
-
-
-
-  return 0;
+    return 0;
 }
-
