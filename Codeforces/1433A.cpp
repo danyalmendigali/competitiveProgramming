20c73f0bee2f1ab49f8f890161e5dc959a7d08bb
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-06-18 23:36:55 +0600

    Codeforcex

--- Codeforces/1433A.cpp
+++ Codeforces/1433A.cpp
@@ -2,10 +2,6 @@
 #include <windows.h>
 using namespace std;
 
-#define ll long long
-#define ar array
-#define FOR(i, a, b) for(int i = a; i < b; i++)
-
 int solve(int x)
 {
     int cnt = ((x % 10) - 1) * 10;
@@ -19,14 +15,11 @@ int solve(int x)
 
 
 int main() {
-    cin.tie(0);
-    SetConsoleOutputCP(CP_UTF8);
-    SetConsoleCP(CP_UTF8);
 
     int t;
     cin >> t;
 
-    while (t--) {
+    while(t--) {
       int x;
       cin >> x;
       cout << solve(x) << "\n";
