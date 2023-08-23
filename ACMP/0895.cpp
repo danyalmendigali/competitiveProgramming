076c370f1d09b0fcdc1eb74b3c6e0562a34c1dd6
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-08-23 22:27:25 +0600

    ACMP

--- ACMP/0895.cpp
+++ ACMP/0895.cpp
@@ -1,1 +1,50 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+
+
+
+int main()
+{
+    cin.tie(0);
+
+    vector<string> matrix(3);
+    for(int i = 0; i < 3; i++) {
+        cin >> matrix[i];
+    }
+
+    for(char ch : {'X', 'O'}) {
+        for(int i = 0; i < 3; i++) {
+            if(matrix[i][0] == ch && matrix[i][1] == ch && matrix[i][2] == ch) {
+                cout << (ch == 'X' ? "Win" : "Lose") << endl;
+                return 0;
+            }
+            if(matrix[0][i] == ch && matrix[1][i] == ch && matrix[2][i] == ch) {
+                cout << (ch == 'X' ? "Win" : "Lose") << endl;
+                return 0;
+            }
+        }
+
+        if(matrix[0][0] == ch && matrix[1][1] == ch && matrix[2][2] == ch) {
+            cout << (ch == 'X' ? "Win" : "Lose") << endl;
+            return 0;
+        }
+
+        if(matrix[0][2] == ch && matrix[1][1] == ch && matrix[2][0] == ch) {
+            cout << (ch == 'X' ? "Win" : "Lose") << endl;
+            return 0;
+        }
+    }
+
+    cout << "Draw" << "\n";
+
+
+
+    return 0;
+}
+
 
