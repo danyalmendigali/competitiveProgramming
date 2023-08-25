05c8aa03136379c836eaa5159744df1d38fb879b
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-08-25 14:13:39 +0600

    ACMP

--- ACMP/0271.cpp
+++ ACMP/0271.cpp
@@ -1,40 +1,51 @@
-#include <bits/stdc++.h>
-#include <iterator>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
 using namespace std;
 
-#define ar array
-#define ll long long
-#define FOR(i, a, b) for(int i = a; i < b; i++)
-
-void solve()
-{
-    int n;
-    cin >> n;
-
-    vector<int> fibonacci(1200000000);
-
-    fibonacci[0] = 0;
-    fibonacci[1] = 1;
-
-    for(int i = 2; i < 1200000000; i++)
-    {
-        fibonacci[i] = fibonacci[i - 2] +  fibonacci[i - 1];
-    }
-
-
-}
 
 
 int main() {
 
+    int n;
+    cin >> n;
 
-    solve();
+    vector<long long int> fibonacci(1000000);
 
+    fibonacci[0] = 0;
+    fibonacci[1] = 1;
 
+    for(int i = 2; i < 1000000; i++)
+    {
+        fibonacci[i] = fibonacci[i - 2] +  fibonacci[i - 1];
+    }
+
+    int b = 1;
+    for(int i = 0; i < fibonacci.size(); i++)
+    {
+        if (fibonacci[i] == n)
+        {
+            cout << "1" << "\n";
+            cout << i << "\n";
+            b = 0;
+            return false;
+        }
+    }
+
+    if(b == 1)
+    {
+        cout << "0" << "\n";
+    }
 
 
 
     return 0;
 }
+
+
+
+
+
 
 
