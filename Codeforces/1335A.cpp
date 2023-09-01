dffdda0ab02a64f250cbd16196bd9c855fda9e9b
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-09-02 00:04:10 +0600

    Competitive Programming

--- Codeforces/1335A.cpp
+++ Codeforces/1335A.cpp
@@ -4,6 +4,7 @@
 
 #define optimus_prime ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 #define all(a) a.begin() , a.end()
+#define fxd(x) fixed << setprecision(x)
 #define endl "\n"
 #define FOR(i, a, b) for(int i = a; i < b; i++)
 #define pb(a) push_back(a)
@@ -19,14 +20,27 @@ const ll mod = 1e9 + 7;
 using namespace std;
 
 void solve() {
-        int a, b, c, x, y;
-        cin >> a >> b >> c >> x >> y;
 
-        if (x <= a && y <= b && (x + y) <= (a + b + c)) {
-            cout << "YES" << endl;
-        } else {
-            cout << "NO" << endl;
+
+
+        int size_1;
+        cin >> size_1;
+
+        int total_ways = 0;
+
+        if (size_1 < 3)
+            cout << total_ways << endl;
+        else {
+            if (size_1 % 2 == 0) {
+                total_ways = size_1 / 2 - 1;
+                cout << total_ways << endl;
+            }
+            else {
+                total_ways = size_1 / 2;
+                cout << total_ways << endl;
+            }
         }
+
 
 
 }
