0a3972cfe53261c3514c282839458b589986e08f
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-09-02 09:50:32 +0600

    Codeforces

--- Codeforces/1692C.cpp
+++ Codeforces/1692C.cpp
@@ -11,25 +11,30 @@ const int SIZE = 8;
 bool solve()
 {
     vector<vector<char>> myVector(SIZE, vector<char>(SIZE));
-    for(int i = 0; i < SIZE; i++)
+    for (int i = 0; i < SIZE; i++)
     {
-        for(int j = 0; j < SIZE; j++)
+        for (int j = 0; j < SIZE; j++)
         {
             cin >> myVector[i][j];
         }
     }
 
-    for(int i = 0; i < SIZE; i++)
+    for (int i = 1; i < SIZE - 1; i++)
     {
-        for(int j = 0; j < SIZE; j++)
+        for (int j = 1; j < SIZE - 1; j++)
         {
-            if(myVector[i - 1][j - 1] == myVector[i - 1][j + 1] && myVector[i - 1][j - 1] == myVector[i + 1][j - 1] && myVector[i - 1][j - 1] == myVector[i + 1][j + 1] && myVector[i + 1][j + 1] == '#')
+            if (myVector[i][j] == '#' &&
+                myVector[i - 1][j - 1] == '#' &&
+                myVector[i - 1][j + 1] == '#' &&
+                myVector[i + 1][j - 1] == '#' &&
+                myVector[i + 1][j + 1] == '#')
             {
                 cout << i + 1 << " " << j + 1 << "\n";
                 return false;
             }
         }
     }
+
     return true;
 }
 
@@ -38,12 +43,8 @@ int main()
     int t;
     cin >> t;
     while (t--)
-    {
+       solve();
 
-    }
 
     return 0;
 }
-
-
-
