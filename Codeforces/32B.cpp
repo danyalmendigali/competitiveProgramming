2a167b1e83c6be14dcd647e2395bbfc58ab2e64a
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-09-14 19:40:23 +0600

    Codeforces

---
+++ Codeforces/32B.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <set>
+
+#define all(a) a.begin() , a.end()
+#define optimus_prime  cin.tie(0); cout.tie(0)
+#define endl "\n"
+#define vll vector<long long>
+#define vi vector<int>
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+#define pb(a) push_back(a)
+#define sz size()
+#define ll long long
+#define F first
+#define S second
+
+using namespace std;
+
+const ll N = 101;
+const ll inf = 1e9 + 9;
+const ll mod = 1e9 + 7;
+
+
+ll a[N][N];
+ll countA = 0;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    for(int i = 0; i < s.sz; i++)
+    {
+        if(s[i] == '.')
+        {
+            cout << 0;
+        }
+        if(s[i] == '-' && s[i + 1] == '.')
+        {
+            cout << 1;
+            i += 1;
+        }
+        if(s[i] == '-' && s[i + 1] == '-')
+        {
+            cout << 2;
+            i += 1 ;
+        }
+    }
+
+
+}
+
+signed main()
+{
+
+    optimus_prime;
+
+    solve();
+
+
+
+
+
+    return 0;
+}
