8ae3ede9de525fcd78ecbb8402dcec4e252a46b0
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-09-26 20:24:14 +0600

    Codeforces

---
+++ Codeforces/733A.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <set>
+
+#define all(a) a.begin() , a.end()
+#define optimus_prime  cin.tie(0); cout.tie(0)
+#define endl "\n"
+#define vll vector<long long>
+#define vi vector<int>
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+#define pb(a) push_back(a)
+#define sz size()
+#define ll long long
+#define F first
+#define S second
+
+using namespace std;
+
+const ll N = 101;
+const ll inf = 1e9 + 9;
+const ll mod = 1e9 + 7;
+
+
+ll a[N][N];
+ll countA = 0;
+
+void solve()
+{
+    ll ans = 1, res = -1;
+    string s;
+    cin >> s;
+    s += 'A';
+    for(int i = 0; i < s.sz; i++)
+    {
+        if(s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U' || s[i] == 'Y')
+        {
+            ans = max(ans, i - res);
+            res = i;
+        }
+    }
+
+    cout << ans << endl;
+
+
+
+}
+
+signed main()
+{
+
+    optimus_prime;
+
+    solve();
+
+
+
+
+
+    return 0;
+}
