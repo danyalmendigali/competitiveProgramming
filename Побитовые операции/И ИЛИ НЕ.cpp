d40481136aeae640386d97a163b61b1219d3a8e5
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-10-03 23:58:40 +0600

    Codeforces

---
+++ Побитовые операции/И ИЛИ НЕ.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+#define optimus_prime ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define all(a) a.begin(), a.end()
+#define endl "\n"
+#define vll vector<long long>
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+#define pb(a) push_back(a)
+#define p(a) push(a)
+#define sz size()
+#define ll long long
+#define ld double
+#define F first
+#define S second
+
+using namespace std;
+
+const int N = 55;
+const int inf = 1e9 + 9;
+const int mod = 1e9 + 7;
+
+void solve()
+{
+    /* Побитовая не(~)
+    char var = 153;
+    char not_v = ~var;
+
+    cout << (int)not_v << endl;
+    */
+
+
+
+    char flags = 5;
+    char mask = 4;
+
+    char res = flags & mask;
+
+    cout << (int)res << endl;
+}
+
+signed main()
+{
+    optimus_prime;
+
+    solve();
+
+
+    return 0;
+}
