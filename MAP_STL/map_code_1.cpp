dd6c3c1c6ecc3ae9e313426a71b12d27fedbe077
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-10-08 23:33:42 +0600

    MAP

---
+++ MAP_STL/map_code_1.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+
+#define all(a) a.begin() , a.end()
+#define optimus_prime  cin.tie(0); cout.tie(0)
+#define endl "\n"
+#define vll vector<int>
+#define vi vector<int>
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+#define pb(a) push_back(a)
+#define sz size()
+#define ll long long
+#define F first
+#define S second
+
+using namespace std;
+
+const ll N = 1e5 + 5;
+const ll inf = 1e9;
+const ll INF = 1e9 + 9;
+const ll mod = 1e9 + 7;
+
+int x[N];
+map<ll, ll> mp;
+
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> x[i];
+        mp[x[i]]++;
+    }
+
+    for (const auto& pair : mp) {
+        cout << pair.first << ": " << pair.second << endl;
+    }
+
+
+}
+
+
+signed main(){
+    optimus_prime;
+
+    int t;
+    t = 1;
+    //cin >> t;
+    while(t--)
+        solve();
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
