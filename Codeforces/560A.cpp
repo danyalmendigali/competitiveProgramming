0ac2f8d2c5b48db715edb583293ede55a181dd5b
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-10-10 00:32:23 +0600

    Codeforces

---
+++ Codeforces/560A.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+#include <windows.h>
+
+#define optimus_prime  cin.tie(0); cout.tie(0)
+#define all(a) a.begin() , a.end()
+#define endl "\n"
+#define vll vector<long long>
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+#define pb(a) push_back(a)
+#define sz size()
+#define ll long long
+#define F first
+#define S second
+
+using namespace std;
+
+const ll N = 101;
+const ll inf = 1e9 + 9;
+const ll mod = 1e9 + 7;
+
+int x[N];
+map<ll, ll> mp;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    for(int i = 0; i < n; i++)
+    {
+        int ans;
+        cin >> ans;
+        if(ans == 1)
+        {
+            cout << -1 << endl;
+            return;
+        }
+    }
+
+    cout << 1 << endl;
+}
+
+signed main()
+{
+    SetConsoleOutputCP(CP_UTF8);
+    SetConsoleCP(CP_UTF8);
+
+    optimus_prime;
+
+    ll t;
+    t = 1;
+    //cin >> t;
+    while(t--)
+        solve();
+
+
+
+    return 0;
+}
