e248ea4b80bec6c7fed810264581a98fab3807cf
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-10-23 20:00:41 +0600

    Codeforces

---
+++ Codeforces/1352C.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <set>
+
+#define all(a) a.begin() , a.end()
+#define optimus_prime  cin.tie(0); cout.tie(0)
+#define endl "\n"
+#define vll vector<long long>
+#define vi vector<int>
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+#define pb(a) push_back(a)
+#define sz size()
+#define ll long long
+#define F first
+#define S second
+
+using namespace std;
+
+const ll N = 101;
+const ll inf = 1e9 + 9;
+const ll mod = 1e9 + 7;
+
+
+ll countA = 0;
+
+set<int> st;
+map<ll, ll> mp;
+
+void solve()
+{
+    mp.clear();
+    ll n, k;
+    cin >> n >> k;
+    ll f = k - 1;
+    ll r = n - 1;
+    ll res = f / r;
+    res += k;
+    cout << res << endl;
+
+
+
+}
+
+signed main()
+{
+    optimus_prime;
+    int t;
+    t = 1;
+    cin >> t;
+    while(t--)
+         solve();
+
+
+
+
+
+    return 0;
+}
