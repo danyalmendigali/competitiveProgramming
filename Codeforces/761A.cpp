9caca135e785a211cd653b524f371d31e8571ab3
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-10-27 19:51:06 +0600

    Codeforces

---
+++ Codeforces/761A.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+#include <cmath>
+
+#define optimus_prime ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define all(a) a.begin(), a.end()
+#define endl "\n"
+#define vll vector<long long>
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+#define pb(a) push_back(a)
+#define p(a) push(a)
+#define sz size()
+#define ll long long
+#define ld double
+#define F first
+#define S second
+
+using namespace std;
+
+const int N = 55;
+const int inf = 1e9 + 9;
+const int mod = 1e9 + 7;
+const int square = 360;
+
+
+void solve()
+{
+    int a, b;
+    cin >> a >> b;
+    if(a == b && a == 0)
+    {
+        cout << "NO" << endl;
+        return;
+    }
+    if(a + 1 == b)
+    {
+        cout << "YES" << endl;
+        return;
+    }
+    if(a == b + 1)
+    {
+        cout << "YES" << endl;
+        return;
+    }
+    if(a == b)
+    {
+        cout << "YES" << endl;
+        return;
+    }
+
+    else
+    {
+        cout << "NO" << endl;
+        return;
+    }
+
+
+}
+
+signed main()
+{
+    optimus_prime;
+
+    ll t;
+    t = 1;
+    //cin >> t;
+    while (t--)
+        solve();
+
+
+    return 0;
+}
