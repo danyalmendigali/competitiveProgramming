943eb07c99fc4dc1f843e6a5bbce81dda368191a
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-10-30 18:04:03 +0600

    Codeforces

---
+++ Codeforces/573A.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <set>
+
+#define all(a) a.begin() , a.end()
+#define ios ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define endl "\n"
+#define vll vector<long long>
+#define vi vector<int>
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+#define pb(a) push_back(a)
+#define sz size()
+#define ll long long
+#define F first
+#define S second
+
+using namespace std;
+
+const ll N = 101;
+const ll inf = 1e9 + 9;
+const ll mod = 1e9 + 7;
+
+
+ll a[N][N];
+ll countA = 0;
+
+set<int> st;
+map<string , int> mp;
+map <ll, ll> mp1 , mp2;
+vector<ll> v, v1;
+
+void solve()
+{
+    mp.clear(); st.clear();
+    int siz; cin >> siz;
+    vector<ll> dp(siz);
+    for(int i = 0; i < siz; i++)
+    {
+        cin >> dp[i];
+        while(dp[i] % 3 == 0) {
+            dp[i] /= 3;
+        }
+        while(dp[i] % 2 == 0) {
+            dp[i] /= 2;
+        }
+    }
+    sort(all(dp));
+
+    if(dp[0] == dp[siz - 1]){
+        cout << "YES" << endl;
+        return;
+    }
+
+    cout << "NO" << endl;
+}
+
+signed main()
+{
+    ios;
+    int t;
+    t = 1;
+    //cin >> t;
+    while(t--)
+         solve();
+
+
+
+
+    return 0;
+}
