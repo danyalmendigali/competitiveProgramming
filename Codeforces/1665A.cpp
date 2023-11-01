7b3bd65bdc04cd29e804c0a1ca895b98818a2d52
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-11-01 19:49:14 +0600

    Codeforces

--- Codeforces/1665A.cpp
+++ Codeforces/1665A.cpp
@@ -1,51 +1,51 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <algorithm>
-#include <set>
+    #include <iostream>
+    #include <string>
+    #include <vector>
+    #include <algorithm>
+    #include <set>
 
-#define all(a) a.begin() , a.end()
-#define optimus_prime  cin.tie(0); cout.tie(0)
-#define endl "\n"
-#define vll vector<long long>
-#define vi vector<int>
-#define FOR(i, a, b) for(int i = a; i < b; i++)
-#define pb(a) push_back(a)
-#define sz size()
-#define ll long long
-#define F first
-#define S second
+    #define all(a) a.begin() , a.end()
+    #define optimus_prime  cin.tie(0); cout.tie(0)
+    #define endl "\n"
+    #define vll vector<long long>
+    #define vi vector<int>
+    #define FOR(i, a, b) for(int i = a; i < b; i++)
+    #define pb(a) push_back(a)
+    #define sz size()
+    #define ll long long
+    #define F first
+    #define S second
 
-using namespace std;
+    using namespace std;
 
-const ll N = 101;
-const ll inf = 1e9 + 9;
-const ll mod = 1e9 + 7;
-
-
-ll a[N][N];
-ll countA = 0;
-
-void solve()
-{
-    int num; cin >> num;
-    cout << num - 3 << " " << 1 << " " << 1 << " " << 1 << endl;
-}
-
-signed main()
-{
-    optimus_prime;
-
-    int t;
-    t = 1;
-    cin >> t;
-
-    while(t--)
-         solve();
+    const ll N = 101;
+    const ll inf = 1e9 + 9;
+    const ll mod = 1e9 + 7;
 
 
+    ll a[N][N];
+    ll countA = 0;
+
+    void solve()
+    {
+        int num; cin >> num;
+        cout << num - 3 << " " << 1 << " " << 1 << " " << 1 << endl;
+    }
+
+    signed main()
+    {
+        optimus_prime;
+
+        int t;
+        t = 1;
+        cin >> t;
+
+        while(t--)
+             solve();
 
 
 
-    return 0;
-}
+
+
+        return 0;
+    }
