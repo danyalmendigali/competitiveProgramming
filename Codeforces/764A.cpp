25480bb162957489018bcffe9c5dda40ee20e7d1
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-11-02 12:12:30 +0600

    Codeforces

---
+++ Codeforces/764A.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <set>
+
+#define all(a) a.begin() , a.end()
+#define ios ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define endl "\n"
+#define vll vector<long long>
+#define vi vector<int>
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+#define pb(a) push_back(a)
+#define sz size()
+#define ll long long
+#define F first
+#define S second
+
+using namespace std;
+
+const ll N = 101;
+const ll inf = 1e9 + 9;
+const ll mod = 1e9 + 7;
+
+
+ll a[N][N];
+ll countA = 0;
+
+set<int> st;
+map<int , int> mp;
+vector<ll> v, v1;
+
+void solve() {
+    int n, m, z;
+    cin >> n >> m >> z;
+    int gcd = __gcd(n, m);
+    int res = n * m;
+    int lcm = res / gcd;
+
+    int ans = z / lcm;
+
+    cout << ans << endl;
+}
+
+signed main()
+{
+    ios;
+    int t;
+    t = 1;
+    //cin >> t;
+    while(t--)
+         solve();
+
+
+
+    return 0;
+}
