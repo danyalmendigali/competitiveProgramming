e8768767f405d7c2aecc69c6beebe629e4d9a0ed
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-11-02 12:28:59 +0600

    Codeforces

---
+++ Codeforces/670A.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <set>
+
+#define all(a) a.begin() , a.end()
+#define ios ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define endl "\n"
+#define vll vector<long long>
+#define vi vector<int>
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+#define pb(a) push_back(a)
+#define sz size()
+#define ll long long
+#define F first
+#define S second
+
+using namespace std;
+
+const ll N = 101;
+const ll inf = 1e9 + 9;
+const ll mod = 1e9 + 7;
+
+
+ll a[N][N];
+ll countA = 0;
+
+set<int> st;
+map<int , int> mp;
+vector<ll> v, v1;
+
+void solve() {
+    int n; cin >> n;
+
+    int mn = (n / 7) * 2;
+    int res = n % 7;
+
+    int mx = mn + min(res, 2);
+
+    if (res == 6)
+        mn++;
+
+    cout << mn << " " << mx << endl;
+}
+
+signed main()
+{
+    ios;
+    int t;
+    t = 1;
+    //cin >> t;
+    while(t--)
+         solve();
+
+
+
+    return 0;
+}
