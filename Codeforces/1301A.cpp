8a88418fa04bfea7aaa1c61fb4c75c8d262672ab
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-11-03 19:00:20 +0600

    Codeforces

---
+++ Codeforces/1301A.cpp
@@ -0,0 +1,69 @@
+
+#include <bits/stdc++.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <set>
+
+#define all(a) a.begin() , a.end()
+#define ios ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define endl "\n"
+#define vll vector<long long>
+#define vi vector<int>
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+#define pb(a) push_back(a)
+#define sz size()
+#define ll long long
+#define F first
+#define S second
+
+using namespace std;
+
+const ll N = 101;
+const ll inf = 1e9 + 9;
+const ll mod = 1e9 + 7;
+
+
+ll a[N][N];
+ll countA = 0;
+
+set<int> st;
+map<string , int> mp;
+map <ll, ll> mp1 , mp2;
+vector<ll> v, v1;
+
+void solve()
+{
+    mp.clear();
+    string a, b, c;
+    int res = 1;
+    cin >> a >> b >> c;
+    for(int i = 0; i < a.sz; i++){
+        if(a[i] != c[i] && b[i] != c[i]){
+            res = 0;
+            break;
+        }
+    }
+
+
+    if(res == 0){
+        cout << "NO" << endl;
+        return;
+    }
+    cout << "YES" << endl;
+}
+
+signed main()
+{
+    ios;
+    int t;
+    t = 1;
+    cin >> t;
+    while(t--)
+         solve();
+
+
+
+
+    return 0;
+}
