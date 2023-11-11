220f7d165c4b0e46fdd52761fb93d6ca5dbb0318
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-11-11 22:26:46 +0600

    Codeforces

---
+++ Codeforces/1325B.cpp
@@ -0,0 +1,59 @@
+
+#include <bits/stdc++.h>
+#include <cmath>
+
+#define optimus_prime ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define all(a) a.begin(), a.end()
+#define endl "\n"
+#define vll vector<long long>
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+#define pb(a) push_back(a)
+#define p(a) push(a)
+#define sz size()
+#define ll long long
+#define ld double
+#define F first
+#define S second
+
+using namespace std;
+
+const int N = 55;
+const int INF = 1e9 + 9;
+const int mod = 1e5 + 9;
+const int square = 360;
+
+map<int, int> mp1;
+map<int, int> mp2;
+set<int> st;
+
+
+void solve()
+{
+    st.clear();
+    int n; cin >> n;
+    vector<int> dp(n);
+    for(int i = 0; i < n; i++){
+        cin >> dp[i];
+        st.insert(dp[i]);
+    }
+    int a = st.sz;
+    cout << a << endl;
+
+
+
+}
+
+signed main()
+{
+    optimus_prime;
+
+    ll t;
+    t = 1;
+    cin >> t;
+    while(t--)
+        solve();
+
+
+    return 0;
+}
+
