dc282ff8c7ad559a1e49001a8ab5d258b41d23e9
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-11-16 23:08:30 +0600

    Codeforces

--- Codeforces/799A.cpp
+++ Codeforces/799A.cpp
@@ -1,62 +1,62 @@
 #include <bits/stdc++.h>
-#include <cmath>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <set>
 
-#define optimus_prime ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
-#define all(a) a.begin(), a.end()
+#define all(a) a.begin() , a.end()
+#define optimus_prime  cin.tie(0); cout.tie(0)
 #define endl "\n"
 #define vll vector<long long>
+#define vi vector<int>
 #define FOR(i, a, b) for(int i = a; i < b; i++)
 #define pb(a) push_back(a)
-#define p(a) push(a)
 #define sz size()
 #define ll long long
-#define ld double
 #define F first
 #define S second
 
 using namespace std;
 
-const int N = 55;
-const int inf = 1e9 + 9;
-const int mod = 1e9 + 7;
-const int square = 360;
+const ll N = 101;
+const ll inf = 1e9 + 9;
+const ll mod = 1e9 + 7;
 
-map<int, int> mp;
-set<pair<int, int>> st;
 
-void solve() {
+ll a[N][N];
+ll countA = 0;
+
+set <string> st;
+map<char, int> mp;
+map<string, int> mp2;
+
+void solve()
+{
     int n, t, k, d;
     cin >> n >> t >> k >> d;
 
-    int time1 = (n + k - 1) / k * t;
-    int time2 = d + (n + 2 * k - 1) / (2 * k) * t;
+    int time1 = ((n + k - 1) / k) * t;
+    int time2 = d + ((n + 2 * k - 1) / (2 * k)) * t;
 
-    if(time1 == time2){
+    if (time2 < time1) {
         cout << "YES" << endl;
-        return;
-    }
-    if(time1 > time2){
-        cout << "YES" << endl;
-        return;
-    }
-    if(time1 < time2){
+    } else {
         cout << "NO" << endl;
-        return;
     }
-
-
 
 }
 
 signed main()
 {
     optimus_prime;
-
-    ll t;
+    int t;
     t = 1;
     //cin >> t;
     while(t--)
-        solve();
+         solve();
+
+
+
 
 
     return 0;
