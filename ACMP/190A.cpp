9b2cc9c3752b20ff679978d7916967b4addaf133
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-11-19 23:33:51 +0600

    Codeforces

---
+++ ACMP/190A.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <set>
+
+#define all(a) a.begin() , a.end()
+#define optimus_prime  cin.tie(0); cout.tie(0)
+#define endl "\n"
+#define vll vector<long long>
+#define vi vector<int>
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+#define pb(a) push_back(a)
+#define sz size()
+#define ll long long
+#define F first
+#define S second
+
+using namespace std;
+
+const ll N = 101;
+const ll inf = 1e9 + 9;
+const ll mod = 1e9 + 7;
+
+
+ll a[N][N];
+ll countA = 0;
+
+void solve()
+{
+    int n, m;
+    cin >> n >> m;
+
+    int mn = max(n, m);
+    int mx = n + max(0, m - 1);
+
+    if (n == 0 && m > 0){
+        cout << "Impossible";
+        return;
+    }
+
+    cout << mn << " " << mx;
+
+}
+
+
+signed main()
+{
+    optimus_prime;
+    int t;
+    t = 1;
+    //cin >> t;
+    while(t--)
+         solve();
+
+
+
+
+
+    return 0;
+}
