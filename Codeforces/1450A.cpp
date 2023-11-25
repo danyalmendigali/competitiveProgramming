72eeb0be7001932df0b06c382d8eeddc008cfcdd
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-11-25 23:02:30 +0600

    Codeforces

---
+++ Codeforces/1450A.cpp
@@ -0,0 +1,57 @@
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <climits>
+
+#define all(a) a.begin() , a.end()
+#define optimus_prime  cin.tie(0); cout.tie(0)
+#define endl "\n"
+#define vll vector<long long>
+#define vi vector<int>
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+#define pb(a) push_back(a)
+#define sz size()
+#define ll long long
+#define F first
+#define S second
+
+using namespace std;
+
+const ll N = 101;
+const ll inf = 1e9 + 9;
+const ll mod = 1e9 + 7;
+
+
+ll a[N][N];
+ll countA = 0;
+
+void solve() {
+    int a; cin >> a;
+    string s;
+    cin >> s;
+    sort(all(s));
+    cout << s << endl;
+
+
+}
+
+signed main()
+{
+    optimus_prime;
+
+    ll t;
+    t = 1;
+    cin >> t;
+
+    while(t--)
+         solve();
+
+
+
+
+
+    return 0;
+}
