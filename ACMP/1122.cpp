182d706727e7629d802295581ff958b2a0636c81
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-11-29 20:22:30 +0600

    Graph

---
+++ ACMP/1122.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+#include <queue>
+#include <algorithm>
+
+#define ll long long
+#define pb(a) push_back(a)
+#define sz size()
+#define F first
+#define S second
+#define optimus_prime ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+using namespace std;
+
+const ll inf = 1e9 + 9;
+const ll mod = 1e9 + 7;
+const ll N = 101;
+
+using vertex = int;
+using Edge = vector<vertex>;
+using Graph = vector<Edge>;
+
+Graph g;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<pair<int, int>> p;
+    g.resize(n, vector<vertex>(n));
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cin >> g[i][j];
+        }
+    }
+
+    int edge = 0;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(g[i][j] == 1){
+                p.emplace_back(i + 1, j + 1);
+                edge++;
+            }
+        }
+    }
+
+    cout << n << " " << edge << endl;
+
+    for(int i = 0; i < p.sz; i++){
+        cout << p[i].F << " " << p[i].S << endl;
+    }
+}
+
+signed main()
+{
+    optimus_prime;
+    int t;
+    //cin >> t;
+    t = 1;
+    while(t--)
+        solve();
+
+
+    return 0;
+}
