b2c54e3444da5c3c4627965eb83b0ab2b513b0d7
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-12-01 21:17:30 +0600

    Codeforces

---
+++ Codeforces/1700B.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+#include <algorithm>
+
+#define ll long long
+#define sz size()
+#define pb(a) push_back(a)
+#define all(dp) dp.begin(), dp.end()
+#define optimus_prime ios::sync_with_stdio(false)
+
+using namespace std;
+
+
+const ll inf = 1e9 + 9;
+const ll INF = 1e15 + 9;
+const ll mod = 1e9 + 7;
+const ll N = 101;
+
+map<int, int> mp;
+set<int> st;
+
+void solve()
+{
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    int r = 0;
+    while (s[r] == '0') {
+        r++;
+    }
+    string str = s.substr(r, n - r);
+    string res = str;
+    for (int i = str.length() - 1; i >= 0; i--) {
+        res += str[i];
+    }
+
+    cout << res << endl;
+}
+
+signed main()
+{
+    optimus_prime;
+    int t;
+    t = 1;
+    cin >> t;
+    while(t--)
+        solve();
+
+
+
+    return 0;
+}
