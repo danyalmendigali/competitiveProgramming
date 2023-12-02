de854e0eda7774848625ad6a3471e3c5fc8089d7
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-12-03 00:12:05 +0600

    Graph

--- task_graph/F.cpp
+++ task_graph/F.cpp
@@ -21,34 +21,40 @@ const ll N = 101;
 
 using vertex = int;
 using Edge = vector<vertex>;
-using Graph = vector<Edge>;
+using GraphVertex = vector<pair<vertex, vertex>>;
+using GraphMatrix = vector<Edge>;
 
-Graph g;
+GraphVertex g;
+GraphMatrix g1;
+
+// Из списка ребер в матрицу
 
 void solve()
 {
-    int a, b;
-    cin >> a >> b;
-    g.resize(a, vector<vertex>(a));
-    for(int i = 0; i < a; i++){
-        for(int j = 0; j < a; j++){
-            g[i][j] = 0;
+    int n, m; cin >> n >> m;
+    g.resize(m);
+    g1.resize(n, vector<vertex>(n));
+
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            g1[i][j] = 0;
         }
     }
-    for(int i = 0; i < b; i++)
-    {
-        int from, to;
-        cin >> from >> to;
-        g[from][to] = 1;
-        g[to][from] = 1;
+
+    for(int i = 0; i < m; i++){
+        int to, from;
+        cin >> to >> from;
+        g1[to - 1][from - 1] = 1;
+        g1[from - 1][to - 1] = 1;
     }
 
-    for(int i = 0; i < g.sz; i++){
-        for(int j = 0; j < g.sz; j++){
-            cout << g[i][j] << " ";
+    for(int i = 0; i < g1.sz; i++){
+        for(int j = 0; j < g1.sz; j++){
+            cout << g1[i][j] << " ";
         }
         cout << endl;
     }
+
 }
 
 signed main()
