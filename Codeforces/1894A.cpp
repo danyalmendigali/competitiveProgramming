76fcbbd4d1b2f44ade100266ac9ea8ba0c1f6094
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-12-03 22:15:36 +0600

    Codeforces

---
+++ Codeforces/1894A.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <set>
+#include <algorithm>
+#include <map>
+#include <cmath>
+
+#define ll long long
+#define sz size()
+#define S second
+#define F first
+#define optimus_prime ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+using namespace std;
+
+const ll N = 101;
+const ll INF = 1e9 + 9;
+const ll MOD = 1e9 + 7;
+
+void solve() {
+    int n; cin >> n;
+    string s; cin >> s;
+    cout << s[s.sz - 1] << endl;
+
+}
+
+signed main()
+{
+    int t;
+    t = 1;
+    cin >> t;
+    while(t--)
+        solve();
+
+
+
+    return 0;
+}
