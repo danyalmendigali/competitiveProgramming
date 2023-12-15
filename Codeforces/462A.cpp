27150007d13b9b40c85a36cc9853148e0e7e2dfc
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-12-15 23:47:20 +0600

    Codeforces

--- Codeforces/462A.cpp
+++ Codeforces/462A.cpp
@@ -1,58 +1,53 @@
-#include <vector>
-#include <string>
 #include <iostream>
+#include <string>
+#include <vector>
 #include <set>
-#include <cmath>
-#include <algorithm>
 #include <map>
+#include <algorithm>
 
 #define ll long long
 #define sz size()
 #define pb(a) push_back(a)
-#define F first
 #define S second
+#define F first
+
+using namespace std;
 
 const ll inf = 1e9 + 9;
 const ll mod = 1e9 + 7;
 const ll N = 101;
 
-using namespace std;
+map<char, int> mp1, mp2;
+
+bool isPrime(int a)
+{
+    int res = 0;
+    for(int i = 1; i <= a; i++){
+        if(a % i == 0) res++;
+    }
+    if(res == 2){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
 
 
 void solve()
 {
-        int n;
-    cin >> n;
-
-    char str[100][100];
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            cin >> str[i][j];
-        }
+    int a, b, res = 0;
+    cin >> a >> b;
+    vector<int> dp(a);
+    for(int i = 0; i < a; i++){
+        cin >> dp[i];
     }
-
-    for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
-            int res = 0;
-            if (i > 0 && str[i - 1][j] == 'o') {
-                res++;
-            }
-            if (i < n - 1 && str[i + 1][j] == 'o') {
-                res++;
-            }
-            if (j > 0 && str[i][j - 1] == 'o') {
-                res++;
-            }
-            if (j < n - 1 && str[i][j + 1] == 'o') {
-                res++;
-            }
-            if (res % 2 != 0) {
-                cout << "NO" << endl;
-                return;
-            }
-        }
+    sort(dp.begin(), dp.end());
+    for(int i = 0; i < a - 1; i++){
+        res += dp[i];
     }
-    cout << "YES" << endl;
+    cout << (res <= b ? "YES" : "NO") << endl;
+
 
 }
 
@@ -63,7 +58,6 @@ signed main()
     //cin >> t;
     while(t--)
         solve();
-
 
 
 
