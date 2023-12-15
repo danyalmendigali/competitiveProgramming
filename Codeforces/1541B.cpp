414de4901d2650f483405674f4eca8442cacdf1e
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-12-15 16:53:21 +0600

    Codeforces

---
+++ Codeforces/1541B.cpp
@@ -0,0 +1,52 @@
+#include <vector>
+#include <set>
+#include <iostream>
+#include <map>
+#include <string>
+#include <algorithm>
+
+#define ll long long
+#define sz size()
+#define pb(a) push_back(a)
+#define S second
+#define F first
+#define all(dp) dp.begin(), dp.end();
+
+using namespace std;
+
+const ll inf = 1e9 + 9;
+const ll mod = 1e9 + 7;
+const ll N = 101;
+
+void solve()
+{
+    int n, res = 0; cin >> n;
+    int dp[n + 1];
+    for(int i = 0; i < n; i++){
+        cin >> dp[i + 1];
+    }
+    for(int i = 1; i <= n; i++)
+    {
+        for(int j = dp[i] - i; j <= n; j += dp[i]){
+            if(j >= 0 && i < j && dp[i] * dp[j] == i + j){
+                res++;
+            }
+        }
+    }
+
+    cout << res << endl;
+
+}
+
+signed main()
+{
+    int t;
+    //t = 1;
+    cin >> t;
+    while(t--)
+        solve();
+
+
+
+    return 0;
+}
