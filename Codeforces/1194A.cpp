bee499a5bb9cd873f2fe10a74dcb51dfeb55f596
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-12-17 23:35:38 +0600

    Codeforces

---
+++ Codeforces/1194A.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <algorithm>
+
+#define ll long long
+#define sz size()
+#define pb(a) push_back(a)
+#define S second
+#define F first
+
+using namespace std;
+
+const ll inf = 1e9 + 9;
+const ll mod = 1e9 + 7;
+const ll N = 101;
+
+set<pair<int, int>> st;
+map<int, int> mp;
+
+void solve() {
+    int a, b; cin >> a >> b;
+    cout << b * 2 << endl;
+}
+
+signed main()
+{
+    int t;
+    t = 1;
+    cin >> t;
+    while(t--)
+        solve();
+
+
+
+    return 0;
+}
