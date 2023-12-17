aab4a3a211ece6ba3bf3b413e2b4297273cfacdc
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-12-17 19:20:12 +0600

    Graph

--- task_graph/M.cpp
+++ task_graph/M.cpp
@@ -21,24 +21,28 @@ set<pair<int, int>> st;
 
 using vertex = int;
 using Edge = vector<vertex>;
-using Graph = vector<vector<vertex>>;
+using Graph = vector<pair<vertex, vertex>>;
+using GraphAdjList = vector<vector<vertex>>;
 
 Graph g;
+GraphAdjList g2;
 
 void solve()
 {
     st.clear();
-    int n, val; cin >> n;
-    g.resize(n + 1);
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            cin >> val;
-            if(val) g[i].push_back(j + 1);
-        }
+    int n, m; cin >> n >> m;
+    g.resize(m);
+    g2.resize(n);
+    for(int i = 0; i < m; i++){
+        cin >> g[i].F >> g[i].S;
+        g2[g[i].F - 1].push_back(g[i].S - 1);
+        g2[g[i].S - 1].push_back(g[i].F - 1);
     }
+    cout << endl;
     for(int i = 0; i < n; i++){
-        cout << g[i].sz << endl ;
+        cout << g2[i].sz << endl;
     }
+
 
 
 
