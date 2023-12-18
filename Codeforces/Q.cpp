99669bd802419d9535452709d7e0fa88ab0e69ea
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2023-12-18 08:21:52 +0600

    Graph

---
+++ Codeforces/Q.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#include <algorithm>
+
+#define ll long long
+#define sz size()
+#define pb(a) push_back(a)
+#define S second
+#define F first
+#define all(dp) dp.begin(), dp.end()
+
+#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+
+using namespace std;
+
+const ll inf = 1e9 + 9;
+const ll mod = 1e9 + 7;
+const ll N = 101;
+
+using vertex = int;
+using Edge = vector<vertex>;
+using Graph = vector<pair<vertex, vertex>>;
+using GraphAdjList = vector<vector<vertex>>;
+
+GraphAdjList g2;
+Graph g;
+
+vector<int> res, res2, ans;
+map<int, int> mp;
+set<int> st, st2;
+
+void solve()
+{
+    st.clear(); mp.clear(); st2.clear();
+    int n, m; cin >> n >> m;
+    g.resize(m);
+    g2.resize(n);
+    for(int i = 0; i < m; i++){
+        cin >> g[i].F >> g[i].S;
+        g2[g[i].F - 1].push_back(g[i].S - 1);
+        g2[g[i].S - 1].push_back(g[i].F - 1);
+    }
+    for(int i = 0; i < n; i++){
+        res.pb(g2[i].sz);
+    }
+    /*
+    for(int i = 0; i < res.sz; i++){
+        cout << res[i] << " ";
+    }
+    */
+
+    res.erase(unique(all(res)), res.begin());
+    if(!res.sz) {
+        cout << "NO" << endl;
+        return;
+    }
+    cout << "YES" << endl;
+
+
+
+
+
+
+
+
+}
+
+signed main()
+{
+    int t;
+    t = 1;
+    //cin >> t;
+    while(t--)
+        solve();
+
+
+
+    return 0;
+}
