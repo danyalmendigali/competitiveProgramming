c6d7696d996418aeb3620b4043ea85382b325ff6
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2024-01-03 14:49:30 +0600

    ACMP

--- ACMP/1204.cpp
+++ ACMP/1204.cpp
@@ -1,39 +1,71 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <queue>
+#include <algorithm>
+
+#define ll long long
+#define sz size()
+#define pb(a) push_bacK(a);
+#define F first
+#define S second
+#define mendigaliTrue ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
 
 using namespace std;
 
-int main() {
-    int size_matrix;
-    cin >> size_matrix;
+const ll inf = 1e9 + 9;
+const ll mod = 1e9 + 7;
+const ll N = 1e5 + 5;
 
-    vector<vector<int>> matrix(size_matrix, vector<int>(size_matrix));
-    for(int i = 0; i < size_matrix; i++) {
-        for(int j = 0; j < size_matrix; j++) {
+using vertex = int;
+using Edge = vector<vertex>;
+using GraphAdjList = vector<vector<vertex>>;
+using GraphPair = vector<pair<vertex, vertex>>;
+
+GraphAdjList g1;
+GraphPair g2;
+
+vector<int> res1, res2, comp;
+map<int, int> mp1, mp2;
+set<int> st1, st2;
+vector<bool> was;
+
+void solve()
+{
+    was.clear(); res1.clear(); comp.clear(); st1.clear();
+    int n, ans1 = 0, ans2 = 0, a1 = 0; cin >> n;
+    vector<vector<int>> matrix(n, vector<int>(n));
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
             cin >> matrix[i][j];
-        }
-    }
-
-    bool is_oriented = true; // Переменная для хранения информации об ориентированности графа
-
-    for(int i = 0; i < size_matrix; i++) {
-        for(int j = 0; j < size_matrix; j++) {
-            // Если есть петля (вершица соединена с самой собой) или симметрия не выполнена
-            if(matrix[i][i] == 1 || matrix[i][j] != matrix[j][i]) {
-                is_oriented = false;
-                break;
+            if(matrix[i][j] == 1){
+                a1++;
+            }
+            if(i == j && matrix[i][j] == 0){
+                ans1++;
+            }
+            if(matrix[i][j] == matrix[j][i] && matrix[i][j] == 1){
+                ans2++;
             }
         }
-        if (!is_oriented) {
-            break;
-        }
     }
+    a1 /= 2;
+    if(ans1 == n && ans2 != a1){
+        cout << "YES" << endl;
+        return;
+    }
+    cout << "NO" << endl;
 
-    if (is_oriented) {
-        cout << "YES" << "\n";
-    } else {
-        cout << "NO" << "\n";
-    }
+}
+
+signed main()
+{
+    int t;
+    t = 1;
+    //cin >> t;
+    while(t--) solve();
 
     return 0;
 }
