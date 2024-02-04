525dde8b93aed330e02e89046928e1ae77399196
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2024-02-05 00:20:18 +0600

    ACMP

--- ACMP/1129.cpp
+++ ACMP/1129.cpp
@@ -1,44 +1,42 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <set>
 #include <map>
-#include <cmath>
-#include <algorithm>
+#include <queue>
+#include <stack>
 
 #define ll long long
 #define sz size()
 #define pb(a) push_back(a)
-#define S second
 #define F first
-#define mendigalitrue ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define S second
+#define all(dp) dp.begin(), dp.end()
 
 using namespace std;
 
+
 void solve()
 {
-    int X, P, Y;
-    cin >> X >> P >> Y;
-    int res = 0;
-
-    while (X < Y) {
-        X += X * P / 100;
-        res++;
+    int x, p, y, cnt = 0; cin >> x >> p >> y;
+    x = x * 100;
+    y = y * 100;
+    p = p + 100;
+    while (x < y)
+    {
+        x = (x * p) / 100;
+        cnt++;
     }
-    cout << res << endl;
+    cout << cnt << endl;
+
 
 }
 
 signed main()
 {
-    mendigalitrue;
     int t;
     t = 1;
     //cin >> t;
-    while(t--)
-        solve();
-
-
+    while(t--)  solve();
 
 
     return 0;
