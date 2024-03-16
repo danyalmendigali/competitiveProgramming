26fec0f0864f83bc5fe23349fda32c92b19e56fa
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2024-03-16 09:58:01 +0500

    ACMP

--- ACMP/1124.cpp
+++ ACMP/1124.cpp
@@ -1,49 +1,41 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <set>
 #include <map>
+#include <set>
+#include <queue>
+#include <stack>
 #include <cmath>
 #include <algorithm>
 
-#define ll long long9
+#define ll long long
 #define sz size()
 #define pb(a) push_back(a)
-#define S second
 #define F first
-#define mendigalitrue ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define S second
+#define ios ios::sync_with_stdio(false);
 
 using namespace std;
-
 
 void solve()
 {
     int x1, y1, x2, y2;
-    cin >> x1 >> y1 >> x2 >> y2;
-    int g = abs(y1 - y2);
-    int g2 = abs(x1 - x2);
-    if(x1 == x2 && g <= 2){
+    cin >> x1 >> y1;
+    cin >> x2 >> y2;
+    if (x1 == x2 && ((y1 >= 2 && y2 == y1 + 1) || (y1 == 2 && y2 == 4))){
         cout << "YES" << endl;
         return;
     }
-    else{
-        cout << "NO" << endl;
-    }
-
-
-
-
+    cout << "NO" << endl;
 }
 
 signed main()
 {
-    mendigalitrue;
+    ios;
     int t;
     t = 1;
     //cin >> t;
-    while(t--)
-        solve();
-
+    while(t--) solve();
 
 
 
