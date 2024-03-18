286b9f45b2322cd6a1558c11488384e09f5b92ab
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2024-03-18 19:51:28 +0500

    ACMP

--- ACMP/0491.cpp
+++ ACMP/0491.cpp
@@ -1,48 +1,73 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <queue>
+#include <unordered_set>
 #include <algorithm>
-#include <fstream>
+
+#define ll long long
+#define sz size()
+#define pb(a) push_back(a)
+#define F first
+#define S second
+#define all(dp) dp.begin(), dp.end()
+#define mendigalitrue ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
 using namespace std;
 
-bool string_fun(string str)
+vector<bool> was;
+vector<int> res1, res2, comp;
+set<int> st1, st2;
+map<int, int> mp1, mp2;
+
+bool isPal(string s)
 {
-    bool t;
-    string s;
-
-    for(int i = str.size() - 1; i >= 0; i--)
-    {
-        s += str[i];
+    bool ok = false;
+    int a = 0;
+    for(int i = 0; i < s.sz; i++){
+        if(s[i] == s[s.sz - i - 1]){
+            a++;
+        }
     }
-
-    if(s != str)
-    {
-        t = 1;
+    if(a == s.sz){
+        ok = true;
     }
-
-    return t;
+    return ok;
 }
 
-int main() {
-    cin.tie(0);
-
-    int k = 0;
-    string str, s, s1;
-    cin >> str;
-
-    bool t = false;
-
-    if(string_fun(str))
-    {
-        cout << str;
+void solve()
+{
+    string s;
+    cin >> s;
+    set<char> st;
+    for(int i = 0; i < s.sz; i++){
+        st.insert(s[i]);
+    }
+    if(st.sz == 1){
+        cout << "NO SOLUTION" << endl;
+        return;
+    }
+    if(isPal(s)){
+        for(int i = 0; i < s.sz - 1; i++){
+            cout << s[i];
+        }
+    }
+    else{
+        cout << s << endl;
     }
 
-    else
-    {
-        cout << "NO SOLUTION" << "\n";
-    }
 
 
+}
 
+signed main()
+{
+    int t;
+    t = 1;
+    //cin >> t;
+    while(t--) solve();
 
     return 0;
 }
