558ec44a1de5ca120ce453b7506fb9bf13dfb159
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2024-04-23 16:25:20 +0500

    ACMP

--- ACMP/0046.cpp
+++ ACMP/0046.cpp
@@ -18,21 +18,87 @@ using namespace std;
 
 void solve()
 {
-    string str = "2.7182818284590452353602875";
     int n; cin >> n;
     if(n == 0){
         cout << 3 << endl;
-        return;
     }
-    for(int i = 0; i < n + 2; i++){
-        cout << str[i];
-        if(str[n + 1] > '4'){
-            int g = (str[n] - '0') + 1;
-            cout << g;
-         return;
-        }
+    else if(n == 1){
+        cout << "2.7" << endl;
     }
-    cout << endl;
+    else if(n == 2){
+        cout << "2.72" << endl;
+    }
+    else if(n == 3){
+        cout <<  "2.718" << endl;
+    }
+    else if(n == 4){
+        cout << "2.7183" << endl;
+    }
+    else if(n == 5){
+        cout << "2.71828" << endl;
+    }
+    else if(n == 6){
+        cout << "2.718282" << endl;
+    }
+    else if(n == 7){
+        cout << "2.7182818" << endl;
+    }
+    else if(n == 8){
+        cout << "2.71828183" << endl;
+    }
+    else if(n == 9){
+        cout <<	"2.718281828" << endl;
+    }
+    else if(n == 10){
+        cout <<	"2.7182818285" << endl;
+    }
+    else if(n == 11){
+        cout << "2.71828182846" << endl;
+    }
+    else if(n == 12){
+        cout << "2.718281828459" << endl;
+    }
+    else if(n == 13){
+        cout << "2.7182818284590" << endl;
+    }
+    else if(n == 14){
+        cout << "2.71828182845905" << endl;
+    }
+    else if(n == 15){
+        cout <<	"2.718281828459045" << endl;
+    }
+    else if(n == 16){
+        cout <<	"2.7182818284590452" << endl;
+    }
+    else if(n == 17){
+        cout << "2.71828182845904524" << endl;
+    }
+    else if(n == 18){
+        cout << "2.718281828459045235" << endl;
+    }
+    else if(n == 19){
+        cout << "2.7182818284590452354" << endl;
+    }
+    else if(n == 20){
+        cout << "2.71828182845904523536" << endl;
+    }
+    else if(n == 21){
+        cout << "2.718281828459045235360" << endl;
+    }
+    else if(n == 22){
+        cout << "2.7182818284590452353603" << endl;
+    }
+    else if(n == 23){
+        cout << "2.71828182845904523536029" << endl;
+    }
+    else if(n == 24){
+        cout << "2.718281828459045235360288" << endl;
+    }
+    else if(n == 25){
+        cout << "2.7182818284590452353602875" << endl;
+    }
+
+
 
 
 }
