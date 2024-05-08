45b0b7db39f1ca0ef4bb0c9df4eff94c17189e2a
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2024-05-08 23:43:28 +0500

    Codeforces

--- Codeforces/1285B.cpp
+++ Codeforces/1285B.cpp
@@ -22,58 +22,34 @@ using namespace std;
 #define fori(i, n) for(int i = 0; i < n; i++)
 #define inf 1e18
 
-int isLengthPermutation(vector<int>& vec)
-{
-       int mx = 0;
-       int g = 0;
-       for(int i = 0; i < vec.sz; i++){
-              if(vec[i] > 0){
-                     g++;
-                     mx = max(mx, g);
-              }
-              else{
-                     mx = max(mx, g);
-                     g = 0;
-              }
-       }
-       return mx;
-}
 
 void solve()
 {
-       ll n, num_pos = 0; cin >> n;
-       ll yasser = 0;
-       ll mx = 0;
-       vector<ll> res;
-       vector<ll> a(n);
-       for(int i = 0; i < a.sz; i++){
-              cin >> a[i];
-              if(a[i] >= 0){
-                     num_pos++;
-              }
-              yasser += a[i];
-       }
-       ll ans = a[0], sum = 0, min_sum = 0, num = 0;
-       for(int i = 0; i < a.sz - 1; i++){
-              sum += a[i];
-              ans = max(ans, sum - min_sum);
-              min_sum = min(min_sum, sum);
-              num++;
+    ll n; cin >> n;
+    vector<int> a(n);
 
-       }
-       cout << ans << " " << yasser << endl;
-       if(ans <= yasser && num_pos == a.sz){
-              if(a[0] != 0 && a[a.sz - 1] != 0){
-                     cout << "YES" << endl;
-                     return;
-              }
+    for(int i = 0; i < n; i++){
+       cin >> a[i];
+    }
+    ll s = 0;
+    for(int i = 0; i < n; i++){
+       s += a[i];
+       if(s <= 0){
               cout << "NO" << endl;
               return;
        }
-       else{
+    }
+    s = 0;
+    for(int i = 0; i < n; i++){
+       s += a[n - 1 - i];
+       if(s <= 0){
               cout << "NO" << endl;
               return;
        }
+    }
+    cout << "YES" << endl;
+
+
 
 }
 
@@ -84,7 +60,7 @@ signed main()
        cout.tie(0);
        int t;
        t = 1;
-       //cin >> t;
+       cin >> t;
        while(t--) solve();
 
 
