33723cd10b8af99c9888562f1eae5f9648694772
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2024-05-19 10:17:55 +0500

    algo

---
+++ algo/algebra/gcd/gcd_method.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#include <numeric>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+#define ll long long
+#define sz size()
+#define pb(a) push_back(a)
+#define F first
+#define S second
+#define all(a) a.begin(), a.end()
+
+void solve()
+{
+       int a, b; cin >> a >> b;
+       cout << __gcd(a, b) << endl;
+
+
+}
+
+signed main()
+{
+       ios::sync_with_stdio(false);
+       cin.tie(0);
+       cout.tie(0);
+       int t;
+       t = 1;
+       //cin >> t;
+       while(t--) solve();
+
+       return 0;
+}
---
+++ algo/algebra/lcm/lcmFirst.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <set>
+#include <map>
+#include <numeric>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+#define ll long long
+#define sz size()
+#define pb(a) push_back(a)
+#define F first
+#define S second
+#define all(a) a.begin(), a.end()
+
+int gcd(int a, int b)
+{
+       if(b == 0) return a;
+       else return gcd(b, a % b);
+}
+
+void solve()
+{
+       int a, b; cin >> a >> b;
+       cout << "Euclid: ";
+       cout << ((a * b) / (gcd(a, b)));
+       cout << endl << endl;
+
+       int a1, b1; cin >> a1 >> b1;
+       cout << "Method: ";
+       cout << ((a1 * b1) / (__gcd(a1, b1))); // <algorithm>
+
+
+}
+
+signed main()
+{
+       ios::sync_with_stdio(false);
+       cin.tie(0);
+       cout.tie(0);
+       int t;
+       t = 1;
+       //cin >> t;
+       while(t--) solve();
+
+       return 0;
+}
