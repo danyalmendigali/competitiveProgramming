7c9fa8c4265ac8474578718f85d9e90424ec49e5
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2024-05-31 22:47:59 +0500

    Codeforces

--- Codeforces/1369B.cpp
+++ Codeforces/1369B.cpp
@@ -1,1 +1,44 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <climits>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <numeric>
+#include <cmath>
+#include <algorithm>
 
+using namespace std;
+
+#define ll long long
+#define sz size()
+#define F first
+#define S second
+#define all(a) a.begin(), a.end()
+#define pb(a) push_back(a)
+
+void solve()
+{
+       int n; cin >> n;
+       string s; cin >> s;
+
+
+}
+
+signed main()
+{
+       ios::sync_with_stdio(false);
+       cin.tie(0);
+       cout.tie(0);
+       int t;
+       t = 1;
+       //cin >> t;
+       while(t--) solve();
+
+
+
+
+       return 0;
+}
--- Codeforces/1537C.cpp
+++ Codeforces/1537C.cpp
@@ -27,6 +27,31 @@ void solve()
        for(int i = 0; i < n; i++){
               cin >> a[i];
        }
+       int mn = 1e9;
+       sort(all(a));
+       for(int i = 0; i < a.sz - 1; i++){
+              mn = min(a[i + 1] - a[i], mn);
+       }
+       for(int i = 0; i < a.sz - 1; i++){
+              if(a[i + 1] - a[i] == mn){
+                     cout << a[i] << " ";
+                     for(int j = i + 2; j < n; j++){
+                            cout << a[j] << " ";
+                     }
+                     for(int j = 0; j < i; j++){
+                            cout << a[j] << " ";
+                     }
+                     cout << a[i + 1] << " ";
+                     break;
+              }
+       }
+
+       /*
+       int n; cin >> n;
+       vector<int> a(n);
+       for(int i = 0; i < n; i++){
+              cin >> a[i];
+       }
        sort(all(a));
        vector<int> b;
        if(n % 2 == 0){
@@ -53,6 +78,7 @@ void solve()
               cout << endl;
               return;
        }
+       */
 
 
 }
@@ -64,7 +90,7 @@ signed main()
        cout.tie(0);
        int t;
        t = 1;
-       //cin >> t;
+       cin >> t;
        while(t--) solve();
 
        return 0;
