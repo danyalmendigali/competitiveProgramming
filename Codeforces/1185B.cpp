5ce76756a149deb8ccc7fb6395508fbe2e9f3fb4
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2024-06-02 16:44:06 +0500

    Codeforces

--- Codeforces/1185B.cpp
+++ Codeforces/1185B.cpp
@@ -19,6 +19,7 @@ using namespace std;
 
 void solve(){
        string s1, s2; cin >> s1 >> s2;
+       /*
        if(s1 == s2){
               cout << "YES" << endl;
               return;
@@ -27,7 +28,53 @@ void solve(){
               cout << "NO" << endl;
               return;
        }
-       vector<pair<char, vector<int>>> g;
+       */
+
+       int res1 = 0;
+       bool ok = false;
+       vector<pair<char, int>> a;
+       vector<pair<char, int>> b;
+       int cnt = 1;
+       for(int i = 0; i < s1.sz; i++){
+              if(s1[i] == s1[i + 1]){
+                     cnt++;
+              }
+              else{
+                     a.push_back({s1[i], cnt});
+                     cnt = 1;
+              }
+       }
+       int cnt2 = 1;
+       for(int i = 0; i < s2.sz; i++){
+              if(s2[i] == s2[i + 1]){
+                     cnt2++;
+              }
+              else{
+                     b.push_back({s2[i], cnt2});
+                     cnt2 = 1;
+              }
+       }
+       if(a.sz != b.sz){
+              cout << "NO" << endl;
+              return;
+       }
+       int f = 0;
+       int f2 = 0;
+       for(int i = 0; i < max(a.sz, b.sz); i++){
+              if(a[i].F == b[i].F && a[i].S <= b[i].S){
+                     f++;
+              }
+       }
+
+       if(f == max(a.sz, b.sz)){
+              ok = true;
+       }
+       if(ok){
+              cout << "YES" << endl;
+              return;
+       }
+       cout << "NO" << endl;
+
 }
 
 
@@ -38,7 +85,7 @@ signed main()
        cout.tie(0);
        int t;
        t = 1;
-       //cin >> t;
+       cin >> t;
        while(t--) solve();
 
 
