bfaebfc56e8743f7692415a02d85069b49445cbc
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2024-06-14 22:03:10 +0500

    Codeforces

--- Codeforces/1538C.cpp
+++ Codeforces/1538C.cpp
@@ -1,66 +1,65 @@
 #include <iostream>
-#include <string>
 #include <vector>
-#include <algorithm>
+#include <string>
 #include <set>
-
-#define all(a) a.begin() , a.end()
-#define optimus_prime  cin.tie(0); cout.tie(0)
-#define endl "\n"
-#define vll vector<long long>
-#define vi vector<int>
-#define FOR(i, a, b) for(int i = a; i < b; i++)
-#define pb(a) push_back(a)
-#define sz size()
-#define ll long long
-#define F first
-#define S second
+#include <queue>
+#include <cmath>
+#include <map>
+#include <algorithm>
 
 using namespace std;
 
-const ll N = 101;
-const ll inf = 1e9 + 9;
-const ll mod = 1e9 + 7;
+#define ll long long
+#define sz size()
+#define F first
+#define S second
+#define pb(a) push_back(a)
+#define all(a) a.begin(), a.end()
 
-
-ll a[N][N];
-ll countA = 0;
-
-void solve()
+ll cnt_pairs(ll n, ll l, ll r, vector<ll>& a)
 {
-    int n, l, r;
-    cin >> n >> l >> r;
+       sort(all(a));
+       ll cnt = 0;
+       for(int i = 0; i < a.sz; i++){
+              ll left = l - a[i];
+              ll right = r - a[i];
 
-    vector<int> dp(n);
-    for (int i = 0; i < n; i++) {
-        cin >> dp[i];
-    }
+              auto left_bn = lower_bound(a.begin() + i + 1, a.end(), left);
+              auto right_rb = upper_bound(a.begin() + i + 1, a.end(), right);
 
-    sort(all(dp));
+              cnt += distance(left_bn, right_rb);
+       }
 
-    int res = 0;
-    for (int i = 0; i < n; i++) {
-        int le = lower_bound(dp.begin() + i + 1, dp.end(), l - dp[i]) - dp.begin();
-        int ri = upper_bound(dp.begin() + i + 1, dp.end(), r - dp[i]) - dp.begin();
-        res += max(0, ri - le);
-    }
 
-    cout << res << endl;
+       return cnt;
 }
+
+
+void solve(){
+       ll n, l, r;
+       cin >> n >> l >> r;
+       vector<ll> a(n);
+       for(int i = 0; i < n; i++){
+              cin >> a[i];
+       }
+       sort(all(a));
+       cout << l - a[0] << endl;
+       cout << r - a[0] << endl;
+       cout << cnt_pairs(n, l, r, a) << endl;
+}
+
+
 
 signed main()
 {
-    optimus_prime;
-
-    int t;
-    t = 1;
-    cin >> t;
-    while(t--)
-         solve();
-
-
+       ios::sync_with_stdio(false);
+       cin.tie(0);
+       cout.tie(0);
+       int t;
+       t = 1;
+       //cin >> t;
+       while(t--) solve();
 
 
-
-    return 0;
+       return 0;
 }
