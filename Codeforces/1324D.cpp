bc4f6d475253a3345c57b98cd720a2c78d8034de
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2024-06-18 15:12:00 +0500

    Codeforces

---
+++ Codeforces/1324D.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <numeric>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+#define ll long long
+#define sz size()
+#define pb(a) push_back(a)
+#define F first
+#define S second
+#define all(a) a.begin(), a.end()
+#define Fast_Code ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define cl clear
+#define ins insert
+#define ers erase
+#define lwb lower_bound
+#define upb upper_bound
+#define pii pair <int, int>
+#define pll pair<long long, long long>
+#define tostr(x) to_string(x)
+#define tonum(s) stoi(s)
+#define fxd(x) fixed << setprecision(x)
+
+const ll MOD = 1e4 + 7;
+
+
+void solve()
+{
+       int n; cin >> n;
+       vector<int> a(n), b(n);
+       for(int i = 0; i < n; i++){
+              cin >> a[i];
+       }
+       for(int i = 0; i < n; i++){
+              cin >> b[i];
+       }
+       vector<int> c;
+       for(int i = 0; i < n; i++){
+              c.pb(a[i] - b[i]);
+       }
+
+       sort(all(c));
+
+       ll cnt = 0;
+       for(int i = 0; i < c.sz; i++){
+              auto it = upb(c.begin() + i + 1, c.end(), -c[i]);
+              cnt += (c.end() - it);
+       }
+
+
+       cout << cnt << endl;
+
+
+}
+
+
+signed main()
+{
+       Fast_Code;
+       int t;
+       t = 1;
+       //cin >> t;
+       while(t--) solve();
+
+
+       return 0;
+}
