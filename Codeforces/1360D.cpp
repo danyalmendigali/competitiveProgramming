2d8df5881f166f7f385ffb1ddda6d977cfb6647f
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2024-06-22 23:45:08 +0500

    Codeforces

--- Codeforces/1360D.cpp
+++ Codeforces/1360D.cpp
@@ -1,57 +1,64 @@
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
+#include <queue>
 #include <map>
 #include <set>
-#include <queue>
+#include <stack>
+#include <numeric>
+#include <cmath>
 #include <algorithm>
+
+using namespace std;
 
 #define ll long long
 #define sz size()
 #define pb(a) push_back(a)
 #define F first
 #define S second
-#define all(dp) dp.begin(), dp.end()
-#define mendigalitrue ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(a) a.begin(), a.end()
+#define Mendigali_Daniyal ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define cl clear
+#define ins insert
+#define ers erase
+#define pii pair <int, int>
+#define pll pair<long long, long long>
+#define tostr(x) to_string(x)
+#define tonum(s) stoi(s)
+#define fxd(x) fixed << setprecision(x)
 
-using namespace std;
-
-vector<bool> was;
-vector<int> res1, res2, comp;
-set<int> st1, st2;
-map<int, int> mp1, mp2;
-
-int findMaxNum(ll num1, ll num2)
+void mendigali()
 {
-    ll mx = num2 - (num2 % num1);
-    while(mx > 0){
-        if(num1 % mx == 0){
-            return mx;
-        }
-        mx -= num1;
-    }
-}
+       ll n, k; cin >> n >> k;
+       if(k >= n){
+              cout << 1 << endl;
+              return;
+       }
+       ll mn = n;
+       for(ll i = 1; i <= sqrt(n); i++){
+              if(n % i == 0){
+                     if(i <= k){
+                            mn = min(mn, n / i);
+                     }
+                     if(n / i <= k){
+                            mn = min(mn, i);
+                     }
+              }
+       }
 
-void solve()
-{
-    ll n, m; cin >> n >> m;
-    if(n == m || n < m){
-        cout << 1 << endl;
-        return;
-    }
-    if(n > m){
-        ll num = findMaxNum(n, m);
-        cout << num << endl;
-        return;
-    }
+       cout << mn << endl;
+
 }
 
 signed main()
 {
-    int t;
-    t = 1;
-    cin >> t;
-    while(t--) solve();
+       Mendigali_Daniyal;
+       int t;
+       t = 1;
+       cin >> t;
+       while(t--) mendigali();
 
-    return 0;
+
+
+       return 0;
 }
