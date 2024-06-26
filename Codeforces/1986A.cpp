b772a0e37fb496115b043a0ef955302e744fc0fc
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2024-06-26 15:18:24 +0500

    Codeforces

---
+++ Codeforces/1986A.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <queue>
+#include <map>
+#include <stack>
+#include <numeric>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+#define ll long long
+#define sz size()
+#define pb(a) push_back(a)
+#define F first
+#define S second
+#define all(a) a.begin(), a.end()
+#define Fast_Code ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define cl clear
+#define ins insert
+#define ers erase
+#define pii pair <int, int>
+#define pll pair<long long, long long>
+#define tostr(x) to_string(x)
+#define tonum(s) stoi(s)
+#define fxd(x) fixed << setprecision(x)
+
+void solve()
+{
+       int a, b, c; cin >> a >> b >> c;
+       cout << min({(abs(a - b) + abs(a - c)), (abs(b - a) + abs(b - c)), (abs(c - a) + (abs(c - b)))}) << endl;
+
+}
+
+
+signed main()
+{
+       Fast_Code;
+       int t;
+       t = 1;
+       cin >> t;
+       while(t--) solve();
+
+
+       return 0;
+}
