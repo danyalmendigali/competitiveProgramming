3e5ce4d8438195b42d6979430c0d24a07297e06d
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2024-07-07 09:46:51 +0500

    Codeforces

---
+++ Codeforces/1249C1.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <numeric>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+#define ll long long
+#define sz size()
+#define pb(a) push_back(a)
+#define F first
+#define S second
+#define all(a) a.begin(), a.end()
+#define Fast_Code ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define cl clear
+#define ins insert
+#define ers erase
+#define pii pair <int, int>
+#define pll pair<long long, long long>
+#define tostr(x) to_string(x)
+#define tonum(s) stoi(s)
+#define fxd(x) fixed << setprecision(x)
+
+
+bool goodNum(int n)
+{
+       while(n > 0){
+              if(n % 3 == 2){
+                     return false;
+              }
+              n /= 3;
+       }
+       return true;
+}
+
+int fnd(int n)
+{
+       while(!goodNum(n)){
+              n++;
+       }
+       return n;
+}
+
+
+void solve()
+{
+       int n; cin >> n;
+       cout << fnd(n) << endl;
+
+
+
+}
+
+
+signed main()
+{
+       Fast_Code;
+       int t;
+       t = 1;
+       cin >> t;
+       while(t--) solve();
+
+
+       return 0;
+}
