9ea20bbb2058cb82cdd20b1d7bcfc296bf75bd01
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2024-07-07 23:00:48 +0500

    Codeforces

--- Codeforces/1922C.cpp
+++ Codeforces/1922C.cpp
@@ -34,14 +34,58 @@ void solve()
        for(int i = 0; i < n; i++){
               cin >> a[i];
        }
-       vector<ll> path1(a.sz), path2(a.sz);
-       for(int i = 0; i < a.sz - 1; i++){
-              path2[i + 1] = path2[i];
-              if(i == 0 || a[i + 1] - a[i] < a[i] - a[i - 1]) path2[i + 1]++;
-              else path[i + 1] += a[i + 1] - a[i];
+       vector<int> path;
+       path.pb(1);
+       for(int i = 1; i < n - 1; i++){
+              if(abs(a[i] - a[i - 1]) < abs(a[i] - a[i + 1])){
+                     path.pb(-1);
+              }
+              else{
+                     path.pb(1);
+              }
+       }
+       path.pb(-1);
+
+       vector<int> pref1(n), pref2(n);
+       pref1[0] = 0;
+       int ind = 0;
+       for(int i = 1; i < path.sz; i++){
+              if(path[i - 1] == 1){
+                     pref1[i] = pref1[i - 1] + 1;
+                     ind++;
+              }
+              if(path[i - 1] == -1){
+                     pref1[i] = pref1[i - 1] + (a[ind + 1] - a[ind]);
+                     ind++;
+              }
        }
 
+       reverse(all(path));
+       reverse(all(a));
+       int ind2 = 0;
+       for(int i = 1; i < path.sz; i++){
+              if(path[i - 1] == -1){
+                     pref2[i] = pref2[i - 1] + 1;
+                     ind2++;
+              }
+              if(path[i - 1] == 1){
+                     pref2[i] = pref2[i - 1] + (a[ind2] - a[ind2 + 1]);
+                     ind2++;
+              }
+       }
+       reverse(all(pref2));
 
+       int m; cin >> m;
+       while(m--){
+              int x, y; cin >> x >> y;
+              x--; y--;
+              if(x < y){
+                     cout << abs(pref1[y] - pref1[x]) << endl;
+              }
+              else{
+                     cout << abs(pref2[y] - pref2[x]) << endl;
+              }
+       }
 
 
 }
