fbac7701317d9c0e009513c99d2c019ab5a53669
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2024-07-07 23:24:17 +0500

    Codeforces

---
+++ cses_problem/3.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <numeric>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+#define ll long long
+#define sz size()
+#define pb(a) push_back(a)
+#define F first
+#define S second
+#define all(a) a.begin(), a.end()
+#define Fast_Code ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define cl clear
+#define ins insert
+#define ers erase
+#define pii pair <int, int>
+#define pll pair<long long, long long>
+#define tostr(x) to_string(x)
+#define tonum(s) stoi(s)
+#define fxd(x) fixed << setprecision(x)
+
+
+void solve()
+{
+       string s; cin >> s;
+       set<char> str;
+       int mx = 1, cnt = 1;
+       for(int i = 1; i < s.sz; i++){
+              if(s[i] == s[i - 1]){
+                     cnt++;
+                     mx = max(mx, cnt);
+              }
+              else{
+                     cnt = 1;
+              }
+       }
+       cout << mx << endl;
+
+
+
+
+}
+
+
+signed main()
+{
+       Fast_Code;
+       int t;
+       t = 1;
+       //cin >> t;
+       while(t--) solve();
+
+
+       return 0;
+}
