a0673e259ee3c4c8152484ba7ae65ab37c585dba
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2024-08-10 09:03:38 +0500

    Codeforces

--- Codeforces/1409B.cpp
+++ Codeforces/1409B.cpp
@@ -1,48 +1,54 @@
 #include <iostream>
 #include <vector>
-#include <set>
-#include <map>
 #include <string>
+#include <map>
+#include <set>
 #include <queue>
-#include <algorithm>
+#include <cmath>
+#include <iomanip>
 
 #define ll long long
-#define pb(a) push_back(a)
 #define sz size()
+#define pb(a) push_back(a)
 #define F first
 #define S second
-#define optimus_prime ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 
 using namespace std;
 
-const ll inf = 1e9 + 9;
-const ll mod = 1e9 + 7;
-const ll N = 101;
-
-
 void solve()
 {
-    int a, b, x, y, n;
-    cin >> a >> b >> x >> y >> n;
+       ll a, b, x, y, n;
+       ll res1, res2;
+       cin >> a >> b >> x >> y >> n;
+       if((a - x) + (b - y) <= n){
+              cout << x * y << endl;
+              return;
+       }
+       else{
+              ll g = min(n, a - x);
+              ll g1 = n - g;
+              ll g2 = min(g1, b - y);
+              res1 = (a - g) * (b - g2);
 
-    int da = min(n, a - x);
-    int db = min(n - da, b - y);
+              ll g3 = min(n, b - y);
+              ll g4 = n - g3;
+              ll g5 = min(g4, a - x);
+              res2 = (a - g5) * (b - g3);
+       }
 
-    long long result1 = (a - da) * 1LL * max(y, b - n);
-    long long result2 = max(x, a - n) * 1LL * (b - db);
-    cout << min(result1, result2) << endl;
+       cout << min(res1, res2) << endl;
+
+
+
 
 }
 
 signed main()
 {
-    optimus_prime;
-    int t;
-    //t = 1;
-    cin >> t;
-    while(t--)
-        solve();
+       int t = 1;
+       cin >> t;
+       while(t--) solve();
 
 
-    return 0;
+       return 0;
 }
