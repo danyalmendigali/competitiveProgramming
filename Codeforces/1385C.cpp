2ee9c6fb8e9729c776a7b70c576009935bc64db6
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2024-08-10 09:52:23 +0500

    Codeforcesa

--- Codeforces/1385C.cpp
+++ Codeforces/1385C.cpp
@@ -1,30 +1,19 @@
 #include <iostream>
-#include <string>
 #include <vector>
-#include <queue>
+#include <string>
 #include <map>
 #include <set>
-#include <numeric>
+#include <queue>
 #include <cmath>
-#include <algorithm>
-
-using namespace std;
+#include <iomanip>
 
 #define ll long long
 #define sz size()
 #define pb(a) push_back(a)
 #define F first
 #define S second
-#define all(a) a.begin(), a.end()
-#define Fast_Code ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
-#define cl clear
-#define ins insert
-#define ers erase
-#define pii pair <int, int>
-#define pll pair<long long, long long>
-#define tostr(x) to_string(x)
-#define tonum(s) stoi(s)
-#define fxd(x) fixed << setprecision(x)
+
+using namespace std;
 
 void solve()
 {
@@ -33,23 +22,26 @@ void solve()
        for(int i = 0; i < n; i++){
               cin >> a[i];
        }
-
-
-       vector<int> b;
-       int mn = 0;
-       for(int i = 0; i < a.sz; i++){
-
+       int res = 0;
+       bool ok = false;
+       for(int i = a.sz - 2; i >= 0; i--){
+              if(a[i] > a[i + 1] && ok){
+                     res = i + 1;
+                     break;
+              }
+              if(a[i] < a[i + 1]) ok = true;
        }
+
+       cout << res << endl;
+
+
 
 }
 
-
 signed main()
 {
-       Fast_Code;
-       int t;
-       t = 1;
-       //cin >> t;
+       int t = 1;
+       cin >> t;
        while(t--) solve();
 
 
