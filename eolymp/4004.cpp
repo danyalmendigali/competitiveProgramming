8e632d718183237752d6f67c944a7bda9f07c0c3
danyalmendigali <107294983+danyalmendigali@users.noreply.github.com>
2024-11-16 16:03:55 +0500

    Codeforces

---
+++ eolymp/4004.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <set>
+#include <numeric>
+#include <cmath>
+#include <algorithm>
+
+using namespace std;
+
+#define ll long long
+#define sz size()
+#define pb(a) push_back(a)
+#define F first
+#define S second
+#define all(a) a.begin(), a.end()
+#define Fast_Code ios::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define cl clear
+#define ins insert
+#define ers erase
+#define pii pair <int, int>
+#define pll pair<long long, long long>
+#define tostr(x) to_string(x)
+#define tonum(s) stoi(s)
+#define fxd(x) fixed << setprecision(x)
+
+const ll N = 1e2;
+const ll mod = 1e9 + 7;
+const ll inf = 1e9 + 9;
+
+vector<ll> g[N];
+bool used[N];
+bool was[N];
+int color[N];
+
+int n, m, v1, v2;
+
+void dfs(int v, int col){
+	color[v] = col;
+	for(int u : g[v]){
+		if(!color[u]){
+			dfs(u, -col);
+		}
+		else if(color[u] != -col){
+			cout << "NO" << endl;
+			exit(0);
+		}
+	}
+}
+
+void solve()
+{
+	int n, m; cin >> n >> m;
+	for(int i = 0; i < m; i++){
+		cin >> v1 >> v2;
+		v1--; v2--;
+		g[v1].pb(v2);
+		g[v2].pb(v1);	
+	}
+	
+	for(int v = 0; v < n; v++){
+		if(!color[v]){
+			dfs(v, 1);
+		}
+	}
+	
+	cout << "YES" << endl;
+	
+	
+
+}
+
+
+signed main()
+{
+       Fast_Code;
+       int t;
+       t = 1;
+       //cin >> t;
+       while(t--) solve();
+
+
+       return 0;
+}
